0d95348fe7e29951bbaead0e4a383e135e48baa6
mnauendo 200882 <mnauend@stud.hs-heilbronn.de>
2020-04-03 17:52:43 +0200

    interface stuff starting
    + starting class SpaceInvaders
    + loading images in project

--- components/src/main/java/de/hhn/it/pp/components/spaceinvaders/InterfaceInSpace.java
+++ components/src/main/java/de/hhn/it/pp/components/spaceinvaders/InterfaceInSpace.java
@@ -1,4 +1,1 @@
-package de.hhn.it.pp.components.spaceinvaders;
-
-public interface InterfaceInSpace {
-}
+package de.hhn.it.pp.components.spaceinvaders;public interface InterfaceInSpace {    void startGame();    void rockets();    void update();    void draw();    void shot();}
