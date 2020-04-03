36509482b7557cfcd3a268743b95cb89160e99f5
Amadeus Zittel <azittel@stud.hs-heilbronn.de>
2020-04-03 13:54:12 +0200

    Create interface parameterclasses and demo programm

---
+++ components/src/main/java/de/hhn/it/pp/components/astarpathfinding/simulator/AStarPathfinding.java
@@ -0,0 +1,39 @@
+package de.hhn.it.pp.components.astarpathfinding.simulator;
+
+import java.awt.Point;
+
+public class AStarPathfinding implements AStarService{
+
+  @Override
+  public void setStartPoint(Point position) throws IllegalArgumentException {
+
+  }
+
+  @Override
+  public void setEndPoint(Point position) throws IllegalArgumentException {
+
+  }
+
+  @Override
+  public void placeObstacle(Point position) throws IllegalArgumentException {
+
+  }
+
+  @Override
+  public void startVisualization() {
+
+  }
+
+  @Override
+  public void stopVisualization() {
+
+  }
+
+  @Override
+  public void reset() {
+
+  }
+
+
+
+}
---
+++ components/src/test/java/de/hhn/it/pp/components/astarpathfinding/DemoAStarUsage.java
@@ -0,0 +1,67 @@
+package de.hhn.it.pp.components.astarpathfinding;
+
+import de.hhn.it.pp.components.astarpathfinding.simulator.AStarPathfinding;
+import de.hhn.it.pp.components.astarpathfinding.simulator.AStarService;
+import java.awt.Point;
+
+public class DemoAStarUsage {
+  private static final org.slf4j.Logger logger =
+    org.slf4j.LoggerFactory.getLogger(DemoAStarUsage.class);
+
+  public static void main(String[] args) throws Exception {
+    // Create the service
+    logger.info(">>> create service");
+    AStarService service = new AStarPathfinding();
+    logger.info("" + service);
+
+    // Add start point
+    logger.info(">>> add start point");
+    Point start = new Point(0, 3);
+    service.setStartPoint(start);
+    logger.info("" + service);
+
+    Thread.sleep(1500);
+
+    // Add end point
+    logger.info(">>> add end point");
+    Point end = new Point(5, 3);
+    service.setEndPoint(end);
+    logger.info("" + service);
+
+    Thread.sleep(1500);
+
+    // Add Obstacles
+    for (int y = 2; y <= 4; y++) {
+      logger.info(">>> add obstacle");
+      Point obstacle = new Point(3, y);
+      service.placeObstacle(obstacle);
+    }
+    logger.info("" + service);
+
+    Thread.sleep(1500);
+
+    // Start visualization
+    logger.info(">>> visualization started");
+    service.startVisualization();
+    logger.info("" + service);
+
+    Thread.sleep(2500);
+
+    // End visualization
+    logger.info(">>> visualization stopped");
+    service.stopVisualization();
+    logger.info("" + service);
+
+    Thread.sleep(1500);
+
+    // Reset
+    logger.info(">>> reset");
+    service.reset();
+    logger.info("" + service);
+
+
+
+  }
+
+
+}
