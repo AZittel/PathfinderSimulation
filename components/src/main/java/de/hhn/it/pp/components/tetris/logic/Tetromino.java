f29f4fa61cd118baceb42a88e974af5254a8a1bb
Violetta Manow <58043958+VioManow@users.noreply.github.com>
2020-04-05 21:55:56 +0200

    Demo and Tetromino fixed

--- components/src/main/java/de/hhn/it/pp/components/tetris/logic/Tetromino.java
+++ components/src/main/java/de/hhn/it/pp/components/tetris/logic/Tetromino.java
@@ -1,16 +1,16 @@
-/**package de.hhn.it.pp.components.tetris.logic;
+package de.hhn.it.pp.components.tetris.logic;
 
 import javafx.scene.paint.Color;
 
 import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
-
+/**
  * This Class used to Create the individual Tetrominos
-
+ */
 public class Tetromino {
 
-    //private TetrominoType type;
+    private TetrominoType type;
     private int x = 0;
     private int y = 0;
     private int size = 0;
@@ -22,7 +22,7 @@ public class Tetromino {
     /**
      * Constructor for Tetrominos. Assigns a random Tetromino Type upon creation.
      * sets the Color accordingly.
-
+     */
     public Tetromino(){
         type = TetrominoType.randomize();
         switch (type){
@@ -49,13 +49,11 @@ public class Tetromino {
                 break;
         }
     }
-*/
 
     /**
      * Used to actually build the Tetrominos on the Field
      * @return The Tetromino
      */
-    /**
     public int[][][] buildTetromino(){
         // TODO: actually make it work
         int[][][] temp = new int[0][0][0];
@@ -65,7 +63,6 @@ public class Tetromino {
     /**
      * Used to rotate the Tetromino in one direction.
      */
-    /**
     public void rotate(){
         rotation++;
 
@@ -77,19 +74,21 @@ public class Tetromino {
     /**
      * Used to organize the Tetromino Types
      */
-    /**
     public enum TetrominoType{
         I,O,Z,S,T,L,J;
 
         /**
          * Randomizes the Tetromino Type
          * @return a random Tetromino
-
+         */
         public static TetrominoType randomize(){
             return values()[ThreadLocalRandom.current().nextInt(0,values().length)];
         }
     }
 
+    public TetrominoType getType() {
+        return type;
+    }
 
     public int[][][] getBounds() {
         return bounds;
@@ -115,4 +114,4 @@ public class Tetromino {
         return size;
     }
 }
-*/
+
