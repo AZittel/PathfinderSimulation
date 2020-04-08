d33feb1f40c419270fbd91196339d0e49f7c50d2
PatAst <58045368+PatAst@users.noreply.github.com>
2020-04-08 16:34:47 +0200

    Updated class AimTrainer.java as well as the interface AimTrainerService.java.

--- components/src/main/java/de/hhn/it/pp/components/aimtrainer/simulation/AimTrainer.java
+++ components/src/main/java/de/hhn/it/pp/components/aimtrainer/simulation/AimTrainer.java
@@ -1,4 +1,24 @@
 package de.hhn.it.pp.components.aimtrainer.simulation;
 
-public class AimTrainer {
+public class AimTrainer implements AimTrainerService {
+
+    @Override
+    public void createMainMenu(){
+
+    }
+
+    @Override
+    public void startGame(){
+
+    }
+
+    @Override
+    public void showHighScoreList(){
+
+    }
+
+    @Override
+    public void exitGame(){
+
+    }
 }
--- components/src/main/java/de/hhn/it/pp/components/aimtrainer/simulation/AimTrainerService.java
+++ components/src/main/java/de/hhn/it/pp/components/aimtrainer/simulation/AimTrainerService.java
@@ -1,5 +1,29 @@
 package de.hhn.it.pp.components.aimtrainer.simulation;
 
+/**
+ * This Interface shows all interaction with the game AimTrainer.
+ *
+ */
+
 public interface AimTrainerService {
 
+    /**
+     * Creates the main menu of the game.
+     */
+    void createMainMenu();
+
+    /**
+     * Creates a window that plays the game.
+     */
+    void startGame();
+
+    /**
+     *  Creates a window that shows all the highscores.
+     */
+    void showHighScoreList();
+
+    /**
+     * Closes the window of the game.
+     */
+    void exitGame();
 }
