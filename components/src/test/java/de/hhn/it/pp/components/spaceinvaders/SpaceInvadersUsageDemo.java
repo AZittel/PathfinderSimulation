4d7906ce5bf1c50d49312a7930712cdd903d56b8
Darko Miklic <dmiklic@stud.hs-heilbronn.de>
2020-04-13 19:10:50 +0200

    SpaceInvadersUsageDemo.java name changed
    Signed-off-by: Darko Miklic <dmiklic@stud.hs-heilbronn.de>

--- components/src/test/java/de/hhn/it/pp/components/spaceinvaders/SpaceInvadersUsageDemo.java
+++ components/src/test/java/de/hhn/it/pp/components/spaceinvaders/SpaceInvadersUsageDemo.java
@@ -8,13 +8,13 @@ import de.hhn.it.pp.components.exceptions.IllegalParameterException;
 import de.hhn.it.pp.components.spaceinvaders.provider.SpaceInvaders;
 import de.hhn.it.pp.components.spaceinvaders.provider.SpaceInvadersService;
 
-class SpaceInvaderServiceUsageDemo {
+public class SpaceInvadersUsageDemo {
     private static final org.slf4j.Logger logger =
-            org.slf4j.LoggerFactory.getLogger(SpaceInvaderServiceUsageDemo.class);
+            org.slf4j.LoggerFactory.getLogger(SpaceInvadersUsageDemo.class);
 
     public void main(String[] args) throws InterruptedException, IllegalParameterException {
         logger.info("create SpaceInvaderService");
-       SpaceInvadersService spaceInvadersService = new SpaceInvaders ();
+        SpaceInvadersService spaceInvadersService = new SpaceInvaders ();
         logger.info("" + spaceInvadersService);
 
         Thread.sleep(3000);
@@ -28,7 +28,7 @@ class SpaceInvaderServiceUsageDemo {
         Thread.sleep(1000);
 
         logger.info("create Player 1");
-        
+
         int yPos =100;
         int xPos =50;
         spaceInvadersService.createPlayer(xPos,yPos);
@@ -53,5 +53,4 @@ class SpaceInvaderServiceUsageDemo {
         Thread.sleep(1000);
     }
 
-       }
-
+}
