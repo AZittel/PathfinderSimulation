3d1dc38d5e1ddfe6b06fcf876abf1c496c506a9b
Dennis Ates <dates@stud.hs-heilbronn.de>
2020-04-14 19:19:09 +0200

    restored README.txt

---
+++ components/src/main/java/de/hhn/it/pp/components/minesweeper/provider/Minesweeper.java
@@ -0,0 +1,24 @@
+package de.hhn.it.pp.components.minesweeper.provider;
+
+public class Minesweeper implements MinesweeperService{
+
+    @Override
+    public void createStartMenu() {
+
+    }
+
+    @Override
+    public void createField(int width, int height) throws IllegalArgumentException {
+
+    }
+
+    @Override
+    public void setBombs(int bombCount) throws IllegalArgumentException {
+
+    }
+
+    @Override
+    public void restart() {
+
+    }
+}
