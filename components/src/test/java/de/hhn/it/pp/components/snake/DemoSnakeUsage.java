d644b0258fc884a9c2240dca1874a92555ecd9dd
Karen Hofele <khofele@stud.hs-heilbronn.de>
2020-04-15 16:54:16 +0200

    Added methods to demo class.

--- components/src/test/java/de/hhn/it/pp/components/snake/DemoSnakeUsage.java
+++ components/src/test/java/de/hhn/it/pp/components/snake/DemoSnakeUsage.java
@@ -1,7 +1,12 @@
 package de.hhn.it.pp.components.snake;
 
-import de.hhn.it.pp.components.snake.simulation.Snake;
-import de.hhn.it.pp.components.snake.simulation.SnakeService;
+import de.hhn.it.pp.components.exceptions.IllegalParameterException;
+import de.hhn.it.pp.components.snake.provider.Item;
+import de.hhn.it.pp.components.snake.provider.Level;
+import de.hhn.it.pp.components.snake.provider.Snake;
+import java.awt.event.KeyEvent;
+
+
 
 /**
  * Creates a non exectubale demo version of Snake.
@@ -17,67 +22,83 @@ public class DemoSnakeUsage {
   /**
    * Creates a demo game.
    */
-  public static void main(String[] args){
+  public static void main(String[] args) throws IllegalParameterException {
 
     // Create the service
     logger.info(">>> create service");
     SnakeService service = new Snake();
-    logger.info("" + service);
+    logger.info("{}", service);
 
-    // Create the main menu
-    logger.info(">>> create the main menu");
-    service.createMainMenu();
-    logger.info("" + service);
+    // Starts the game, creates the map, spawns snake
+    logger.info(">>> start game");
+    service.startGame("horst64");
+    logger.info("{}", service);
 
-     // Starts the game, creates the map, spawns snake
-     logger.info(">>> start game");
-     service.startGame();
-     logger.info("" + service);
+    Level levelOne = new Level(1, 0);
 
-     // Spawns item
-     logger.info(">>> spawns item");
-     service.spawn(23,49);
-     logger.info("" + service);
+    // Runs the first level
+    logger.info(">>> starts level 1");
+    service.runLevel(levelOne, levelOne.getNeededHighscore());
+    logger.info("{}", service);
 
-     // Snake moves up
-     logger.info(">>> snake moves up");
-     service.move(1);
-     logger.info("" + service);
+    // Returns list of all registered players
+    logger.info(">>> returns the list of all registered players");
+    service.getPlayers();
+    logger.info("{}", service);
 
-     // Snake moves left
-     logger.info(">>> snake moves left");
-     service.move(2);
-     logger.info("" + service);
+    // Returns all information of a player
+    logger.info(">>> returns all information about horst64");
+    service.getPlayer("horst64");
+    logger.info("{}", service);
 
-     // Snake collects item
-     logger.info(">>> snake collects item");
-     service.collect();
-     logger.info("" + service);
+    SnakePlayerDiscriptor snakePlayerDiscriptor = new SnakePlayerDiscriptor("horst64");
 
-     // Snake moves right
-     logger.info(">>> snake moves right");
-     service.move(3);
-     logger.info("" + service);
+    // Adds a listener to a player to get updates on the progress
+    logger.info(">>> adds listener to horst64");
+    service.addCallback("horst64", snakePlayerDiscriptor);
+    logger.info("{}", service);
 
-     // Snake moves down
-     logger.info(">>> snake moves down");
-     service.move(4);
-     logger.info("" + service);
+    // Removes the listener from player
+    logger.info(">>> removes listener from horst64");
+    service.removeCallback("horst64", snakePlayerDiscriptor);
+    logger.info("{}", service);
 
-     // Snake collides and dies
-     logger.info(">>> snake collides with obstacle and dies");
-     service.collide();
-     logger.info("" + service);
+    // Snake moves up
+    logger.info(">>> snake moves up");
+    //service.moveSnake();
+    logger.info("{}", service);
 
-     // Shows highscore
-     logger.info(">>> shows highscore");
-     service.showHighscore();
-     logger.info("" + service);
+    // Snake moves left
+    logger.info(">>> snake moves left");
+    //service.moveSnake();
+    logger.info("{}", service);
 
-     // Exits game
-     logger.info(">>> exits the game");
-     service.exitGame();
-     logger.info("" + service);
- }
+    Item snakeFood = new Item(2);
+
+    // Snake collects item
+    logger.info(">>> snake collects item");
+    service.collect(snakeFood);
+    logger.info("{}", service);
+
+    // Snake moves right
+    logger.info(">>> snake moves right");
+    //service.moveSnake();
+    logger.info("{}", service);
+
+    // Snake moves down
+    logger.info(">>> snake moves down");
+    //service.moveSnake();
+    logger.info("{}", service);
+
+    // Snake collides and dies
+    logger.info(">>> snake collides with obstacle and dies");
+    service.collide();
+    logger.info("{}", service);
+
+    // Exits game
+    logger.info(">>> exits the game");
+    service.exitGame();
+    logger.info("{}", service);
+  }
 
 }
