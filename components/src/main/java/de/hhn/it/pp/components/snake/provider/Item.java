9e45f1ff3b9d21182309d99667b1b1ca6bd55d20
Karen Hofele <khofele@stud.hs-heilbronn.de>
2020-04-15 17:06:56 +0200

    Added class level and snake. Optimized class snake and implemented methods.

---
+++ components/src/main/java/de/hhn/it/pp/components/snake/provider/Item.java
@@ -0,0 +1,40 @@
+package de.hhn.it.pp.components.snake.provider;
+
+/**
+ * Creates items that can be collected by the snake.
+ *
+ * @author Karen Hofele, Sören Greiner
+ */
+
+public class Item {
+
+  /**
+   * Item's value.
+   */
+  private int value;
+
+  /**
+   * Constructor for items.
+   * @param value Item's value
+   */
+  public Item(int value) {
+    this.value = value;
+  }
+
+  /**
+   * Getter for the item's value.
+   * @return item's value
+   */
+  public int getValue() {
+    return this.value;
+  }
+
+  /**
+   * Spawns the item.
+   * @param xPos x-coordinate of the spawnplace
+   * @param yPos y-coordinate of the spawnplace
+   */
+  public void spawn(int xPos, int yPos){
+
+  }
+}
