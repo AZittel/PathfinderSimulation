1748a8024b22863615498df3b0ae298467e4fad8
Benjamin Gloß <bgloss@stud.hs-heilbronn.de>
2020-04-18 22:16:54 +0200

    Commend the Minesweeper.java

--- components/src/main/java/de/hhn/it/pp/components/minesweeper/provider/Minesweeper.java
+++ components/src/main/java/de/hhn/it/pp/components/minesweeper/provider/Minesweeper.java
@@ -1,22 +1,30 @@
 package de.hhn.it.pp.components.minesweeper.provider;
 
 public class Minesweeper implements MinesweeperService{
-
+    /**
+     * creates a start menu.
+     */
     @Override
     public void createStartMenu() {
 
     }
-
+    /**
+     * creates a game field.
+     */
     @Override
     public void createField(int width, int height) throws IllegalArgumentException {
 
     }
-
+    /**
+     * Sets bombs on random locations on the field.
+     */
     @Override
     public void setBombs(int bombCount) throws IllegalArgumentException {
 
     }
-
+    /**
+     *  resets the field and restarts the game with new bombs
+     */
     @Override
     public void restart() {
 
