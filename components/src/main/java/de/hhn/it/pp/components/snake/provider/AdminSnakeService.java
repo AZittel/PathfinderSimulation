66a7cee7327643f9cc06806699e3a984785b0a46
Karen Hofele <khofele@stud.hs-heilbronn.de>
2020-04-22 02:45:50 +0200

    Added javadoc

--- components/src/main/java/de/hhn/it/pp/components/snake/provider/AdminSnakeService.java
+++ components/src/main/java/de/hhn/it/pp/components/snake/provider/AdminSnakeService.java
@@ -1,24 +1,29 @@
 package de.hhn.it.pp.components.snake.provider;
 
-import de.hhn.it.pp.components.snake.SnakePlayerDescriptor;
 import de.hhn.it.pp.components.exceptions.IllegalParameterException;
+import de.hhn.it.pp.components.snake.SnakePlayerDescriptor;
+
 
 /**
  * Admin interface to the OurSnakeService to add / remove a Player to / from the service.
+ *
+ * @author Sören Greiner, Karen Hofele
  */
 public interface AdminSnakeService {
 
-    /**
-     * Adds a new CoffeeMaker to the service.
-     * @param descriptor descriptor of the new coffee maker
-     * @throws IllegalParameterException if the descriptor is a null reference or incomplete.
-     */
-    void addPlayer(SnakePlayerDescriptor descriptor) throws IllegalParameterException;
+  /**
+   * Adds a new player to the game.
+   *
+   * @param nickName of the new player.
+   * @throws IllegalParameterException if the descriptor is a null reference or incomplete.
+   */
+  void addPlayer(SnakePlayerDescriptor nickName) throws IllegalParameterException;
 
-    /**
-     * Removes a CoffeeMaker from the service.
-     * @param coffeeMakerId id of the coffee maker to be removed
-     * @throws IllegalParameterException if the id of the coffee maker does not exist.
-     */
-    void removePlayer(int coffeeMakerId) throws IllegalParameterException;
+  /**
+   * Removes a player from the game.
+   *
+   * @param nickName of the player that should be removed.
+   * @throws IllegalParameterException if the nickname of the player does not exist.
+   */
+  void removePlayer(SnakePlayerDescriptor nickName) throws IllegalParameterException;
 }
