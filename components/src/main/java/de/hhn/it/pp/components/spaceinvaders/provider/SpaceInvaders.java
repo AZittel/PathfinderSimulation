534c150f8bec0a49bec14178ec0de70973f085aa
Darko Miklic <dmiklic@stud.hs-heilbronn.de>
2020-04-22 19:26:37 +0200

    Improvement suggestions from Prof. Winckler's Confluence comment added to the component.
    Signed-off-by: Darko Miklic <dmiklic@stud.hs-heilbronn.de>

--- components/src/main/java/de/hhn/it/pp/components/spaceinvaders/provider/SpaceInvaders.java
+++ components/src/main/java/de/hhn/it/pp/components/spaceinvaders/provider/SpaceInvaders.java
@@ -9,6 +9,8 @@ import de.hhn.it.pp.components.spaceinvaders.Enemies;
 import de.hhn.it.pp.components.spaceinvaders.Field;
 import de.hhn.it.pp.components.spaceinvaders.Player;
 
+import java.util.List;
+
 public class SpaceInvaders implements SpaceInvadersService {
 
     @Override
@@ -21,11 +23,37 @@ public class SpaceInvaders implements SpaceInvadersService {
     public void createEnemies(int xPosition, int yPosition, int speed) {new Enemies();}
 
     @Override
-    public void movement(int xMovement, int yMovement, boolean shot) {
+    public void movement(int xMovement, int yMovement) throws IllegalArgumentException {
+
     }
 
     @Override
-    public void showHighscore(int score) {
+    public void shooting(boolean shoot) {
+
+    }
+
+    @Override
+    public void collide(boolean collision) throws GameOverException {
+
+    }
+
+    @Override
+    public void addHighScore(List<String> highscoreList) {
+
+    }
+
+    @Override
+    public void getHighscoreList(List<String> highscoreList) {
+
+    }
+
+    @Override
+    public void getPositionPlayer(int xPosition, int yPosition) throws IllegalArgumentException {
+
+    }
+
+    @Override
+    public void getPositionEnemies(int xPosition, int yPosition, int speed) throws IllegalArgumentException {
 
     }
 
