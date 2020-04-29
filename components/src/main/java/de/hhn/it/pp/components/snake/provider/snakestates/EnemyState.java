963f91362e811de1fe278e98407474fd885f49e2
Karen Hofele <khofele@stud.hs-heilbronn.de>
2020-04-29 23:18:54 +0200

    Optimized class EnemyState after coding conventions

--- components/src/main/java/de/hhn/it/pp/components/snake/provider/snakestates/EnemyState.java
+++ components/src/main/java/de/hhn/it/pp/components/snake/provider/snakestates/EnemyState.java
@@ -2,44 +2,45 @@ package de.hhn.it.pp.components.snake.provider.snakestates;
 
 import de.hhn.it.pp.components.snake.State;
 import de.hhn.it.pp.components.snake.provider.OurSnake;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 //todo javadoc
 public class EnemyState extends ControlState {
-    private static final org.slf4j.Logger logger =
-            org.slf4j.LoggerFactory.getLogger(EnemyState.class);
+  private static final Logger logger =
+            LoggerFactory.getLogger(EnemyState.class);
 
-    /**
-     * Standard constructor for States.
-     *
-     * @param snake related maker
-     */
-    public EnemyState(final OurSnake snake) {
-        super(snake);
-        state = State.CREATE_ENEMY;
-    }
+  /**
+   * Standard constructor for States.
+   *
+   * @param snake related maker
+   */
+  public EnemyState(final OurSnake snake) {
+    super(snake);
+    state = State.CREATE_ENEMY;
+  }
 
-    @Override
-    public void onStartLevel() throws IllegalStateException {
-    }
+  @Override
+  public void onStartLevel() throws IllegalStateException {
+  }
 
-    @Override
-    public void onEndLevel() throws IllegalStateException {
-    }
+  @Override
+  public void onEndLevel() throws IllegalStateException {
+  }
 
-    @Override
-    public void onCreateEnemy() throws IllegalStateException {
-    }
+  @Override
+  public void onCreateEnemy() throws IllegalStateException {
+  }
 
-    @Override
-    public void onCreateItem() throws IllegalStateException {
-    }
+  @Override
+  public void onCreateItem() throws IllegalStateException {
+  }
 
-    @Override
-    public void onCreateLevel() throws IllegalStateException {
-    }
+  @Override
+  public void onCreateLevel() throws IllegalStateException {
+  }
 
-    @Override
-    public void onMove() throws IllegalStateException {
-
-    }
+  @Override
+  public void onMove() throws IllegalStateException {
+  }
 }
