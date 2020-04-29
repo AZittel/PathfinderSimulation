363f88878190083772976c8f63089d8021db8c56
Karen Hofele <khofele@stud.hs-heilbronn.de>
2020-04-29 23:29:23 +0200

    Optimized class Move after coding conventions

--- components/src/main/java/de/hhn/it/pp/components/snake/Move.java
+++ components/src/main/java/de/hhn/it/pp/components/snake/Move.java
@@ -1,26 +1,29 @@
 package de.hhn.it.pp.components.snake;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 //todo javadoc
 public class Move {
-    private static final org.slf4j.Logger logger =
-            org.slf4j.LoggerFactory.getLogger(Move.class);
+  private static final Logger logger =
+            LoggerFactory.getLogger(Move.class);
 
-    private Direction direction;
+  private Direction direction;
 
-    /**
-     * Constructor for the Move direction of the snake.
-     *
-     * @param direction direction in there the snake moves
-     */
-    public Move(Direction direction) {
-        this.direction = direction;
-    }
+  /**
+   * Constructor for the Move direction of the snake.
+   *
+   * @param direction direction in there the snake moves
+   */
+  public Move(Direction direction) {
+    this.direction = direction;
+  }
 
-    public Direction getDirection() {
-        return direction;
-    }
+  public Direction getDirection() {
+    return direction;
+  }
 
-    public void setDirection(Direction direction) {
-        this.direction = direction;
-    }
+  public void setDirection(Direction direction) {
+    this.direction = direction;
+  }
 }
