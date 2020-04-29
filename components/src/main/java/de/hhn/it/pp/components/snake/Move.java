a4eafb5336b18239647b1577af55ab87e106ddac
Karen Hofele <khofele@stud.hs-heilbronn.de>
2020-04-29 22:03:01 +0200

    Added todos to Move

--- components/src/main/java/de/hhn/it/pp/components/snake/Move.java
+++ components/src/main/java/de/hhn/it/pp/components/snake/Move.java
@@ -1,5 +1,6 @@
 package de.hhn.it.pp.components.snake;
 
+//todo javadoc
 public class Move {
     private static final org.slf4j.Logger logger =
             org.slf4j.LoggerFactory.getLogger(Move.class);
