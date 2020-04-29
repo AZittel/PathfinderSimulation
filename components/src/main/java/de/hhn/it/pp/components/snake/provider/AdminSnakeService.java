a09e7cf60b8342eb5790b8992c46060db6870f8d
Karen Hofele <khofele@stud.hs-heilbronn.de>
2020-04-29 23:29:42 +0200

    Optimized class AdminSnakeService after coding conventions

--- components/src/main/java/de/hhn/it/pp/components/snake/provider/AdminSnakeService.java
+++ components/src/main/java/de/hhn/it/pp/components/snake/provider/AdminSnakeService.java
@@ -1,30 +1,30 @@
 package de.hhn.it.pp.components.snake.provider;
 
-import de.hhn.it.pp.components.snake.SnakePlayerDescriptor;
 import de.hhn.it.pp.components.exceptions.IllegalParameterException;
+import de.hhn.it.pp.components.snake.SnakePlayerProfile;
 
 //todo checkstyle
 
 /**
-        * Admin interface to the OurSnakeService to add / remove a Player to / from the service.
-        *
-        * @author Sören Greiner, Karen Hofele
-        */
+ * Admin interface to the OurSnakeService to add / remove a Player to / from the service.
+ *
+ * @author Sören Greiner, Karen Hofele
+ */
 public interface AdminSnakeService {
 
   /**
-          * Adds a new player to the game.
-            *
-            * @param descriptor of the new player.
+   * Adds a new player to the game.
+   *
+   * @param descriptor of the new player.
    * @throws IllegalParameterException if the descriptor is a null reference or incomplete.
    */
-    void addPlayer(SnakePlayerDescriptor descriptor) throws IllegalParameterException;
+  void addPlayer(SnakePlayerProfile descriptor) throws IllegalParameterException;
 
-    /**
-     * Removes a player from the game.
-     *
-     * @param nickname of the player that should be removed.
-     * @throws IllegalParameterException if the nickname of the player does not exist.
-     */
-    void removePlayer(String nickname) throws IllegalParameterException;
+  /**
+   * Removes a player from the game.
+   *
+   * @param nickname of the player that should be removed.
+   * @throws IllegalParameterException if the nickname of the player does not exist.
+   */
+  void removePlayer(String nickname) throws IllegalParameterException;
 }
