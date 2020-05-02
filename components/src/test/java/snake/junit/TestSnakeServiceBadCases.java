1e163de136d32a5aaf96190797568f4d9253f711
Karen Hofele <khofele@stud.hs-heilbronn.de>
2020-05-02 14:23:26 +0200

    refactored names

--- components/src/test/java/snake/junit/TestSnakeServiceBadCases.java
+++ components/src/test/java/snake/junit/TestSnakeServiceBadCases.java
@@ -1,6 +1,6 @@
 package snake.junit;
 
-import de.hhn.it.pp.components.snake.SnakePlayerDescriptor;
+import de.hhn.it.pp.components.snake.SnakePlayerProfile;
 import de.hhn.it.pp.components.snake.SnakePlayerService;
 import de.hhn.it.pp.components.snake.provider.AdminSnakePlayerService;
 import de.hhn.it.pp.components.snake.provider.OurSnakePlayerService;
@@ -8,21 +8,24 @@ import de.hhn.it.pp.components.exceptions.IllegalParameterException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+//todo!!!
 @DisplayName("Test the SnakePlayerService with bad cases.")
 public class TestSnakeServiceBadCases {
-    private static final org.slf4j.Logger logger =
-            org.slf4j.LoggerFactory.getLogger(de.hhn.it.pp.components.example.coffeemakerservice.junit.TestCoffeMakerServiceBadCases.class);
+    private static final Logger logger =
+            LoggerFactory.getLogger(de.hhn.it.pp.components.example.coffeemakerservice.junit.TestCoffeMakerServiceBadCases.class);
 
     SnakePlayerService snakePlayerService;
     AdminSnakePlayerService adminSnakePlayerService;
 
     @BeforeEach
-    void setup(List<SnakePlayerDescriptor> descriptors) throws IllegalParameterException {
+    void setup(List<SnakePlayerProfile> descriptors) throws IllegalParameterException {
         OurSnakePlayerService ourSnakePlayerService = new OurSnakePlayerService();
         snakePlayerService = ourSnakePlayerService;
         adminSnakePlayerService = ourSnakePlayerService;
@@ -33,7 +36,7 @@ public class TestSnakeServiceBadCases {
     void testExceptionWhenRequestingNonExistentPlayer() {
         IllegalParameterException illegalParameterException = assertThrows(
                 IllegalParameterException.class,
-                () -> snakePlayerService.getSnakePlayer(23));
+                () -> snakePlayerService.getSnakePlayerProfile(23));
     }
 
     @Test
