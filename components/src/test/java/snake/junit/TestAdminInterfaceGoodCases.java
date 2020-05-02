b742806fd94af22f571904a501e4cec4112e15ba
Karen Hofele <khofele@stud.hs-heilbronn.de>
2020-05-02 14:22:18 +0200

    formulated names more precisely; added test as prefix to method names

--- components/src/test/java/snake/junit/TestAdminInterfaceGoodCases.java
+++ components/src/test/java/snake/junit/TestAdminInterfaceGoodCases.java
@@ -1,7 +1,6 @@
 package snake.junit;
 
-import de.hhn.it.pp.components.example.coffeemakerservice.CoffeeMakerDescriptor;
-import de.hhn.it.pp.components.snake.SnakePlayerDescriptor;
+import de.hhn.it.pp.components.snake.SnakePlayerProfile;
 import de.hhn.it.pp.components.snake.SnakePlayerService;
 import de.hhn.it.pp.components.snake.provider.AdminSnakePlayerService;
 import de.hhn.it.pp.components.snake.provider.OurSnake;
@@ -10,7 +9,8 @@ import de.hhn.it.pp.components.exceptions.IllegalParameterException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
@@ -18,15 +18,15 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayName("Test the admin interface with good cases.")
 public class TestAdminInterfaceGoodCases {
-    private static final org.slf4j.Logger logger =
-            org.slf4j.LoggerFactory.getLogger(TestAdminInterfaceGoodCases.class);
+    private static final Logger logger =
+            LoggerFactory.getLogger(TestAdminInterfaceGoodCases.class);
 
     SnakePlayerService snakePlayerService;
     AdminSnakePlayerService adminSnakePlayerService;
-    List<SnakePlayerDescriptor> descriptor;
+    List<SnakePlayerProfile> descriptor;
 
     @BeforeEach
-    void setup (List<SnakePlayerDescriptor> descriptor) {
+    void setup (List<SnakePlayerProfile> descriptor) {
         OurSnake.setIdCounter(0);
         OurSnakePlayerService ourSnakePlayerService = new OurSnakePlayerService();
         snakePlayerService = ourSnakePlayerService;
@@ -36,20 +36,20 @@ public class TestAdminInterfaceGoodCases {
 
     @Test
     @DisplayName("A new instance has no player.")
-    public void aNewInstanceHasNoPlayers() {
-        List<SnakePlayerDescriptor>
-                players = snakePlayerService.getSnakePlayers();
+    public void testANewInstanceHasNoPlayers() {
+        List<SnakePlayerProfile>
+                players = snakePlayerService.getAllSnakePlayerProfiles();
         assertNotNull(players);
         assertEquals(0, players.size(), "The list should be empty.");
     }
 
     @Test
     @DisplayName("Adding one player to the service results in one entry.")
-    public void addOnePlayer() throws IllegalParameterException {
-        SnakePlayerDescriptor descriptor = new SnakePlayerDescriptor("LongDongSilver");
-        adminSnakePlayerService.addSnakePlayer(descriptor);
-        List<SnakePlayerDescriptor> players = snakePlayerService.getSnakePlayers();
-        SnakePlayerDescriptor descriptor1 = players.get(0);
+    public void testAddOnePlayer() throws IllegalParameterException {
+        SnakePlayerProfile descriptor = new SnakePlayerProfile("LongDongSilver");
+        adminSnakePlayerService.addSnakePlayerProfile(descriptor);
+        List<SnakePlayerProfile> players = snakePlayerService.getAllSnakePlayerProfiles();
+        SnakePlayerProfile descriptor1 = players.get(0);
         assertAll(
                 () -> assertEquals(1, players.size(), "There should be exactly one player in the list."),
                 () -> assertEquals(0, descriptor1.getId(), "Id of players should start with 0")
@@ -58,13 +58,13 @@ public class TestAdminInterfaceGoodCases {
 
     @Test
     @DisplayName("adds and remove one player and check the result")
-    public void removePlayer() throws IllegalParameterException {
-        SnakePlayerDescriptor descriptor = new SnakePlayerDescriptor("LongDongSilver");
-        adminSnakePlayerService.addSnakePlayer(descriptor);
-        List<SnakePlayerDescriptor> players = snakePlayerService.getSnakePlayers();
-        SnakePlayerDescriptor descriptor1 = players.get(0);
-        adminSnakePlayerService.removeSnakePlayer(players.get(0).getId());
-        players = snakePlayerService.getSnakePlayers();
+    public void testRemovePlayer() throws IllegalParameterException {
+        SnakePlayerProfile descriptor = new SnakePlayerProfile("LongDongSilver");
+        adminSnakePlayerService.addSnakePlayerProfile(descriptor);
+        List<SnakePlayerProfile> players = snakePlayerService.getAllSnakePlayerProfiles();
+        SnakePlayerProfile descriptor1 = players.get(0);
+        adminSnakePlayerService.removeSnakePlayerProfile(players.get(0).getId());
+        players = snakePlayerService.getAllSnakePlayerProfiles();
         assertEquals(0, players.size());
         }
 }
