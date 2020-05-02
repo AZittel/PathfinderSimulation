4358095554c1d4dd5b9541f0a054c1d544940cd9
Karen Hofele <khofele@stud.hs-heilbronn.de>
2020-05-02 13:06:18 +0200

    Optimized code after coding conventions

--- components/src/main/java/de/hhn/it/pp/components/snake/State.java
+++ components/src/main/java/de/hhn/it/pp/components/snake/State.java
@@ -2,5 +2,5 @@ package de.hhn.it.pp.components.snake;
 
 //todo javadoc
 public enum State {
-     CREATING_LEVEL, GAME_OVER, LEVEL_READY , MOVING, COLLIDED, COLLECTING, GROWING
+     CREATING_LEVEL, GAME_OVER, LEVEL_READY, MOVING, COLLIDED, COLLECTING, GROWING
 }
