97c2eed497abd10df0fc6ae0c4ba7ff265e08916
Karen Hofele <khofele@stud.hs-heilbronn.de>
2020-05-02 13:01:39 +0200

    Optimized code after coding conventions

--- components/src/main/java/de/hhn/it/pp/components/snake/provider/AdminSnakePlayerService.java
+++ components/src/main/java/de/hhn/it/pp/components/snake/provider/AdminSnakePlayerService.java
@@ -1,28 +1,28 @@
 package de.hhn.it.pp.components.snake.provider;
 
-import de.hhn.it.pp.components.snake.SnakePlayerDescriptor;
 import de.hhn.it.pp.components.exceptions.IllegalParameterException;
+import de.hhn.it.pp.components.snake.SnakePlayerDescriptor;
 
 /**
-        * Admin interface to the OurSnakeService to add / remove a Player to / from the service.
-        *
-        * @author Sören Greiner, Karen Hofele
-        */
+ * Admin interface to the OurSnakeService to add / remove a Player to / from the service.
+ *
+ * @author Sören Greiner, Karen Hofele
+ */
 public interface AdminSnakePlayerService {
 
   /**
-          * Adds a new player to the game.
-            *
-            * @param descriptor of the new player.
+   * Adds a new player to the game.
+   *
+   * @param descriptor of the new player.
    * @throws IllegalParameterException if the descriptor is a null reference or incomplete.
    */
-    void addSnakePlayer(SnakePlayerDescriptor descriptor) throws IllegalParameterException;
+  void addSnakePlayer(SnakePlayerDescriptor descriptor) throws IllegalParameterException;
 
-    /**
-     * Removes a player from the game.
-     *
-     * @param snakePlayerId id of the player that should be removed.
-     * @throws IllegalParameterException if the nickname of the player does not exist.
-     */
-    void removeSnakePlayer(int snakePlayerId) throws IllegalParameterException;
+  /**
+   * Removes a player from the game.
+   *
+   * @param snakePlayerId id of the player that should be removed.
+   * @throws IllegalParameterException if the nickname of the player does not exist.
+   */
+  void removeSnakePlayer(int snakePlayerId) throws IllegalParameterException;
 }
