444c8dbec049438a859759aac09b02a4fb5cd7ab
Karen Hofele <khofele@stud.hs-heilbronn.de>
2020-05-02 13:05:48 +0200

    Optimized code after coding conventions

--- components/src/main/java/de/hhn/it/pp/components/snake/provider/Snake.java
+++ components/src/main/java/de/hhn/it/pp/components/snake/provider/Snake.java
@@ -1,26 +1,23 @@
 package de.hhn.it.pp.components.snake.provider;
 
-import de.hhn.it.pp.components.snake.Direction;
+import de.hhn.it.pp.components.exceptions.IllegalParameterException;
+import de.hhn.it.pp.components.snake.Move;
 import de.hhn.it.pp.components.snake.SnakePlayerDescriptor;
 import de.hhn.it.pp.components.snake.SnakePlayerListener;
-import de.hhn.it.pp.components.snake.Move;
-import de.hhn.it.pp.components.exceptions.IllegalParameterException;
-
-import java.awt.event.KeyEvent;
 
 public interface Snake {
 
-    void startGame() throws IllegalStateException;
+  void startGame() throws IllegalStateException;
 
-    void switchLevel() throws IllegalParameterException;
+  void switchLevel() throws IllegalParameterException;
 
-    void endGame() throws IllegalStateException;
+  void endGame() throws IllegalStateException;
 
-    void move(Move direction) throws IllegalParameterException, IllegalStateException;
+  void move(Move direction) throws IllegalParameterException, IllegalStateException;
 
-    void addCallback(SnakePlayerListener listener) throws IllegalParameterException;
+  void addCallback(SnakePlayerListener listener) throws IllegalParameterException;
 
-    void removeCallback(SnakePlayerListener listener) throws IllegalParameterException;
+  void removeCallback(SnakePlayerListener listener) throws IllegalParameterException;
 
-    SnakePlayerDescriptor getDescriptor();
+  SnakePlayerDescriptor getDescriptor();
 }
