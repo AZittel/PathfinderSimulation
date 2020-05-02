c869f6dd6c9f6299ce675c569d8800b9013ffd51
Karen Hofele <khofele@stud.hs-heilbronn.de>
2020-05-02 14:22:02 +0200

    formulated names more precisely; added test as prefix to method names

--- components/src/test/java/snake/junit/TestAdminInterfaceBadCases.java
+++ components/src/test/java/snake/junit/TestAdminInterfaceBadCases.java
@@ -1,6 +1,6 @@
 package snake.junit;
 
-import de.hhn.it.pp.components.snake.SnakePlayerDescriptor;
+import de.hhn.it.pp.components.snake.SnakePlayerProfile;
 import de.hhn.it.pp.components.snake.SnakePlayerService;
 import de.hhn.it.pp.components.snake.provider.AdminSnakePlayerService;
 import de.hhn.it.pp.components.snake.provider.OurSnakePlayerService;
@@ -8,13 +8,15 @@ import de.hhn.it.pp.components.exceptions.IllegalParameterException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @DisplayName("Test admin interface with bad cases.")
 public class TestAdminInterfaceBadCases {
-    private static final org.slf4j.Logger logger =
-        org.slf4j.LoggerFactory.getLogger(TestAdminInterfaceBadCases.class);
+    private static final Logger logger =
+        LoggerFactory.getLogger(TestAdminInterfaceBadCases.class);
 
     SnakePlayerService snakePlayerService;
     AdminSnakePlayerService adminSnakePlayerService;
@@ -28,33 +30,33 @@ public class TestAdminInterfaceBadCases {
 
     @Test
     @DisplayName("Test bad creation of maker with null reference as descriptor")
-    public void createPlayerWithNullReference() throws IllegalParameterException {
+    public void testCreatePlayerWithNullReference() throws IllegalParameterException {
         IllegalParameterException exception = assertThrows(IllegalParameterException.class,
-                () -> adminSnakePlayerService.addSnakePlayer(null));
+                () -> adminSnakePlayerService.addSnakePlayerProfile(null));
     }
 
     @Test
     @DisplayName("Test bad creation with null reference as nickname in descriptor")
-    public void createPlayerWithDescriptorWithNullLocation() {
-        SnakePlayerDescriptor descriptor = new SnakePlayerDescriptor(null);
+    public void testCreatePlayerWithDescriptorWithNullLocation() {
+        SnakePlayerProfile descriptor = new SnakePlayerProfile(null);
         IllegalParameterException exception = assertThrows(IllegalParameterException.class,
-                () -> adminSnakePlayerService.addSnakePlayer(descriptor));
+                () -> adminSnakePlayerService.addSnakePlayerProfile(descriptor));
     }
 
     @Test
     @DisplayName("Test bad creation with empty string as nickname in descriptor")
-    public void createPlayerWithDescriptorWithEmptyStringLocation() {
-        SnakePlayerDescriptor descriptor = new SnakePlayerDescriptor("");
+    public void testCreatePlayerWithDescriptorWithEmptyStringLocation() {
+        SnakePlayerProfile descriptor = new SnakePlayerProfile("");
         IllegalParameterException exception = assertThrows(IllegalParameterException.class,
-                () -> adminSnakePlayerService.addSnakePlayer(descriptor));
+                () -> adminSnakePlayerService.addSnakePlayerProfile(descriptor));
     }
 
     @Test
     @DisplayName("Test bad creation with just white space as location in descriptor")
-    public void createPlayerWithDescriptorWithWhitespaceLocation() {
-        SnakePlayerDescriptor descriptor = new SnakePlayerDescriptor("  ");
+    public void testCreatePlayerWithDescriptorWithWhitespaceLocation() {
+        SnakePlayerProfile descriptor = new SnakePlayerProfile("  ");
         IllegalParameterException exception = assertThrows(IllegalParameterException.class,
-                () -> adminSnakePlayerService.addSnakePlayer(descriptor));
+                () -> adminSnakePlayerService.addSnakePlayerProfile(descriptor));
     }
 
     @Test
@@ -62,7 +64,7 @@ public class TestAdminInterfaceBadCases {
     void testExceptionWhenRemovingNonExistentSnakePlayer() {
         IllegalParameterException illegalParameterException = assertThrows(
                 IllegalParameterException.class,
-                () -> adminSnakePlayerService.removeSnakePlayer(123456));
+                () -> adminSnakePlayerService.removeSnakePlayerProfile(123456));
     }
 
 
