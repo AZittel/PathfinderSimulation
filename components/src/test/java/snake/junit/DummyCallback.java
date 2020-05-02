f93997e33e2efaaf87f2eb4e1954ea5987ffd246
Karen Hofele <khofele@stud.hs-heilbronn.de>
2020-05-02 14:21:24 +0200

    imported Logger and LoggerFactory

--- components/src/test/java/snake/junit/DummyCallback.java
+++ components/src/test/java/snake/junit/DummyCallback.java
@@ -2,10 +2,12 @@ package snake.junit;
 
 import de.hhn.it.pp.components.snake.SnakePlayerListener;
 import de.hhn.it.pp.components.snake.State;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class DummyCallback implements SnakePlayerListener {
-    private static final org.slf4j.Logger logger =
-            org.slf4j.LoggerFactory.getLogger(DummyCallback.class);
+    private static final Logger logger =
+            LoggerFactory.getLogger(DummyCallback.class);
 
     @Override
     public void newState(State state) { logger.info("Callback called with state " + state);
