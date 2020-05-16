365b3f73def3665ee025551d4400af249f72dc81
Oliver Koch <okoch@stud.hs-heilbronn.de>
2020-05-16 18:30:08 +0200

    Added logging

--- components/src/main/java/de/hhn/it/pp/components/craftingservice/provider/Callback.java
+++ components/src/main/java/de/hhn/it/pp/components/craftingservice/provider/Callback.java
@@ -11,6 +11,8 @@ import de.hhn.it.pp.components.exceptions.IllegalParameterException;
  * @version 2020-05-10
  */
 public class Callback implements CraftingListener {
+  private static final org.slf4j.Logger logger =
+      org.slf4j.LoggerFactory.getLogger(CraftingListener.class);
 
   @Override
   public void craftingStartedNotification(
@@ -18,7 +20,7 @@ public class Callback implements CraftingListener {
     if (pattern == null) {
       throw new IllegalParameterException("Crafting pattern was null reference!\n");
     }
-    System.out.println("\n'" + pattern.getName() + "' has started crafting!\n");
+    logger.info("craftingStartedNotification: pattern = {}", pattern);
   }
 
   @Override
@@ -26,6 +28,6 @@ public class Callback implements CraftingListener {
     if (pattern == null) {
       throw new IllegalParameterException("Crafting pattern was null reference!\n");
     }
-    System.out.println("\n'" + pattern.getName() + "' has finished crafting!");
+    logger.info("craftingEndedNotification: pattern = {}", pattern);
   }
 }
