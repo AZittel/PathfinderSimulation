f6ec03bb9505a7fe9c30dc2b8947ce407e74c8ee
Benjamin Gloß <bgloss@stud.hs-heilbronn.de>
2020-05-18 13:25:18 +0200

    starting tests

---
+++ components/src/test/java/de/hhn/it/pp/components/minesweeper/junit/BombTest.java
@@ -0,0 +1,6 @@
+package de.hhn.it.pp.components.minesweeper.junit;
+
+public class BombTest {
+    private static final org.slf4j.Logger logger =
+            org.slf4j.LoggerFactory.getLogger(BombTest.class);
+}
---
+++ components/src/test/java/de/hhn/it/pp/components/minesweeper/junit/FieldTest.java
@@ -0,0 +1,6 @@
+package de.hhn.it.pp.components.minesweeper.junit;
+
+public class FieldTest {
+    private static final org.slf4j.Logger logger =
+            org.slf4j.LoggerFactory.getLogger(FieldTest.class);
+}
