6a6ae12638ba2e4a7e89c9e66ad74bc24e3887d6
Karen Hofele <khofele@stud.hs-heilbronn.de>
2020-05-23 18:07:14 +0200

    Added class TestLevelGoodCases

---
+++ components/src/test/java/de/hhn/it/pp/components/snake/junit/TestLevelGoodCases.java
@@ -0,0 +1,12 @@
+package de.hhn.it.pp.components.snake.junit;
+
+import de.hhn.it.pp.components.snake.provider.OurSnakeLevel;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.platform.commons.logging.Logger;
+import org.junit.platform.commons.logging.LoggerFactory;
+
+@DisplayName("Tests the level class with good cases")
+public class TestLevelGoodCases {
+    private static final Logger logger = LoggerFactory.getLogger(TestLevelGoodCases.class);
+    private OurSnakeLevel level;
+}
