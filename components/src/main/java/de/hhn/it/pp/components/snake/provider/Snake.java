709292378a99f3f313e44dd3aeebacb6e9cb9289
Karen Hofele <khofele@stud.hs-heilbronn.de>
2020-05-30 16:28:24 +0200

    added javadoc renamed id to playerId added logging messages

--- components/src/main/java/de/hhn/it/pp/components/snake/provider/Snake.java
+++ components/src/main/java/de/hhn/it/pp/components/snake/provider/Snake.java
@@ -1,6 +1,5 @@
 package de.hhn.it.pp.components.snake.provider;
 
-import de.hhn.it.pp.components.snake.Direction;
 import de.hhn.it.pp.components.snake.Movement;
 import de.hhn.it.pp.components.snake.SnakePlayerProfile;
 
@@ -10,9 +9,15 @@ import org.slf4j.LoggerFactory;
 // todo javadoc
 //todo logger
 public class Snake {
+
+  /** Snake's Logger */
   private static final Logger logger =
             LoggerFactory.getLogger(Snake.class);
+
+  /** Counter for profile id's */
   private static int idCounter;
+
+  /** variable for player's profile */
   private SnakePlayerProfile profile;
 
   /**
@@ -24,24 +29,26 @@ public class Snake {
   public Snake(SnakePlayerProfile profile) {
     logger.info("Constructor - {}", profile);
     this.profile = profile;
-    profile.setId(idCounter++);
+    profile.setPlayerId(idCounter++);
   }
 
   public static int getIdCounter() {
-    logger.info("getter for id");
+    logger.info("getter for id counter");
     return idCounter;
   }
 
   public static void setIdCounter(final int idCounter) {
+    logger.info("setter for id counter - {}", idCounter);
     Snake.idCounter = idCounter;
   }
 
   public SnakePlayerProfile getProfile() {
+    logger.info("Getter for profile: {}", profile);
     return profile;
   }
 
   public void endGame(){
-
+    //logger.info();
   }
 
   // todo implementieren oder weglassen?
