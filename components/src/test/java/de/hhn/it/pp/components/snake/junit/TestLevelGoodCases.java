aaacfba23a03f8e33ad29ec805c176f31d2b5896
Karen Hofele <khofele@stud.hs-heilbronn.de>
2020-05-30 20:27:24 +0200

    added test methods

--- components/src/test/java/de/hhn/it/pp/components/snake/junit/TestLevelGoodCases.java
+++ components/src/test/java/de/hhn/it/pp/components/snake/junit/TestLevelGoodCases.java
@@ -1,12 +1,34 @@
 package de.hhn.it.pp.components.snake.junit;
 
 import de.hhn.it.pp.components.snake.provider.OurSnakeLevel;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.platform.commons.logging.Logger;
 import org.junit.platform.commons.logging.LoggerFactory;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 @DisplayName("Tests the level class with good cases")
 public class TestLevelGoodCases {
     private static final Logger logger = LoggerFactory.getLogger(TestLevelGoodCases.class);
-    private OurSnakeLevel level;
+    private OurSnakeLevel testLevel;
+
+    @BeforeEach
+    void setup() {
+        OurSnakeLevel testLevel = new OurSnakeLevel(2, 20);
+    }
+
+    @Test
+    @DisplayName("get level's id")
+    void testGetLevelId(){
+        assertEquals(2, testLevel.getLevelId(), "id should be 2");
+    }
+
+    @Test
+    @DisplayName("get needed highscore")
+    void testGetNeededHighscore(){
+        assertEquals(20, testLevel.getNeededHighscore(), "needed highscore should be 20");
+    }
 }
