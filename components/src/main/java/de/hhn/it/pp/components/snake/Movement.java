d078135d2307d71d38059e5d5673d8ee37e0751d
Karen Hofele <khofele@stud.hs-heilbronn.de>
2020-05-30 16:07:45 +0200

    Added logging messages and javadoc

--- components/src/main/java/de/hhn/it/pp/components/snake/Movement.java
+++ components/src/main/java/de/hhn/it/pp/components/snake/Movement.java
@@ -3,11 +3,15 @@ package de.hhn.it.pp.components.snake;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-//todo javadoc
-//todo logger
+/**
+ * Class for snake's movement
+ */
 public class Movement {
+
+  /** Snake's Logger */
   private static final Logger logger = LoggerFactory.getLogger(Movement.class);
 
+  /** current direction of the snake */
   private Direction currentDirection;
 
   /**
@@ -16,14 +20,27 @@ public class Movement {
    * @param currentDirection direction in there the snake moves
    */
   public Movement(Direction currentDirection) {
+    logger.info("Constructor - {}", currentDirection);
     this.currentDirection = currentDirection;
   }
 
+  /**
+   * Getter for current direction
+   *
+   * @return current direction of the snake
+   */
   public Direction getCurrentDirection() {
+    logger.info("getter for currentDirection: {}", currentDirection);
     return currentDirection;
   }
 
+  /**
+   * Setter for current direction
+   *
+   * @param direction new current direction of the snake
+   */
   public void setCurrentDirection(Direction direction) {
+    logger.info("setter for current direction: {}", direction);
     currentDirection = direction;
   }
 }
