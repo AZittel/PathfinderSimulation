aee4e5893fee4fee935624207d364b33536f6f9a
Karen Hofele <khofele@stud.hs-heilbronn.de>
2020-05-30 20:12:07 +0200

    refactored class name

--- components/src/test/java/de/hhn/it/pp/components/snake/junit/TestMovementGoodCases.java
+++ components/src/test/java/de/hhn/it/pp/components/snake/junit/TestMovementGoodCases.java
@@ -1,7 +1,7 @@
 package de.hhn.it.pp.components.snake.junit;
 
 import de.hhn.it.pp.components.snake.Direction;
-import de.hhn.it.pp.components.snake.Move;
+import de.hhn.it.pp.components.snake.Movement;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -11,20 +11,19 @@ import org.junit.platform.commons.logging.LoggerFactory;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @DisplayName("Test the move class with good cases")
-public class TestMoveGoodCases {
-    private static final Logger logger = LoggerFactory.getLogger(TestMoveGoodCases.class);
-
-    Direction direction = Direction.UP;
-    Move move;
+public class TestMovementGoodCases {
+    private static final Logger logger = LoggerFactory.getLogger(TestMovementGoodCases.class);
+    private Direction direction = Direction.UP;
+    private Movement move;
 
     @BeforeEach
     void setup(){
-        move = new Move(direction);
+        move = new Movement(direction);
     }
 
     @Test
     @DisplayName("get current direction")
-    void testGetDirection(){
+    void testGetDirection() {
         assertEquals(direction, move.getCurrentDirection(), "direction should be UP");
     }
 }
