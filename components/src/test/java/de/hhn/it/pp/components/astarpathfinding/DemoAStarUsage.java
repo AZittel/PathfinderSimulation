253449ab136138e8608b3e7aa3d96d342fbcbf6a
Noah Kleinschmidt <nklein@stud.hs-heilbronn.de>
2020-06-01 11:30:22 +0200

    Updated demo

--- components/src/test/java/de/hhn/it/pp/components/astarpathfinding/DemoAStarUsage.java
+++ components/src/test/java/de/hhn/it/pp/components/astarpathfinding/DemoAStarUsage.java
@@ -2,17 +2,11 @@ package de.hhn.it.pp.components.astarpathfinding;
 
 import de.hhn.it.pp.components.astarpathfinding.provider.Pathfinder;
 import de.hhn.it.pp.components.astarpathfinding.provider.Terrain;
-import java.sql.SQLOutput;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class DemoAStarUsage {
   private static final org.slf4j.Logger logger =
       org.slf4j.LoggerFactory.getLogger(DemoAStarUsage.class);
-
-
-
 
   public static void main(String[] args) throws Exception {
     // Create the service
@@ -20,45 +14,47 @@ public class DemoAStarUsage {
     Pathfinder service = new Pathfinder();
     logger.info("" + service);
 
-    // Add start point
-    logger.info(">>> add start point");
+    // Create a 20x20 map
+    logger.info(">>> create a new 20x20 map");
+    service.createMap(20, 20);
+    logger.info("" + service);
+
+    Thread.sleep(500);
+
+    // Set start point
+    logger.info(">>> set start point");
     Position start = new Position(0, 3);
     service.setStartPoint(start);
-    logger.info("" + service);
+    logger.info("Startpoint set to: " + start);
 
-    Thread.sleep(1500);
+    Thread.sleep(500);
 
-    // Add end point
-    logger.info(">>> add end point");
+    // Set end point
+    logger.info(">>> set end point");
     Position end = new Position(5, 3);
     service.setEndPoint(end);
-    logger.info("" + service);
+    logger.info("Endpoint set to: " + end);
 
-    Thread.sleep(1500);
+    Thread.sleep(500);
 
     // Add Obstacles
     for (int y = 2; y <= 4; y++) {
-      logger.info(">>> place terrain at: " + 3 + "|" + y);
+      logger.info(">>> place lava terrain at: " + 3 + "|" + y);
       service.placeTerrain(TerrainType.LAVA, new Position(y, 3));
+      logger.info(String.format("Terrain placed at %d | %d:", 3, y) + TerrainType.LAVA);
+      Thread.sleep(500);
     }
-    logger.info("" + service);
 
-    Thread.sleep(1500);
-
-    // Start visualization
-    logger.info(">>> visualization started");
+    // Start pathfinding algorithm
+    logger.info(">>> pathfinding started");
     List<PathfindingInformation> results = service.doPathfinding();
-    for(Terrain terrain : results.get(results.size()-1).getFinalPathPositions()){
-      System.out.print(terrain + " ");
-    }
-    System.out.println();
-    logger.info("" + service);
+    Thread.sleep(1000);
+    logger.info("Shortest path found: " +  results.get(results.size() - 1).getFinalPathPositions().toString());
 
-    Thread.sleep(2500);
-
+    Thread.sleep(1000);
 
     // Reset
-    logger.info(">>> reset");
+    logger.info(">>> reset map");
     service.reset();
     logger.info("" + service);
   }
