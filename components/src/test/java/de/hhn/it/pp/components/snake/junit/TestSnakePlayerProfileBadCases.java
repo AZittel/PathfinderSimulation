25095e23de1d8408f51bf47c63726012ac04485c
Karen Hofele <khofele@stud.hs-heilbronn.de>
2020-06-03 00:06:13 +0200

    added todo

--- components/src/test/java/de/hhn/it/pp/components/snake/junit/TestSnakePlayerProfileBadCases.java
+++ components/src/test/java/de/hhn/it/pp/components/snake/junit/TestSnakePlayerProfileBadCases.java
@@ -9,6 +9,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @DisplayName("Tests SnakePlayerProfile with bad cases")
 public class TestSnakePlayerProfileBadCases {
@@ -16,7 +17,6 @@ public class TestSnakePlayerProfileBadCases {
     private static final Logger logger = LoggerFactory.getLogger(TestSnakePlayerProfileBadCases.class);
     private SnakePlayerProfile testProfile;
     private int playerId = 1;
-    private int newPlayerId = 1;
 
     @BeforeEach
     void setup() throws IllegalParameterException {
@@ -24,10 +24,12 @@ public class TestSnakePlayerProfileBadCases {
         testProfile.setPlayerId(playerId);
     }
 
+    //todo Frage: Wieso failed der Test?
     /**@Test
     @DisplayName("Tests setPlayerId with id that is already taken")
-    void testSetPlayerIdBadCase() throws IllegalParameterException {
+    void testSetPlayerIdBadCase() {
         IllegalParameterException exception = assertThrows(IllegalParameterException.class,
-                () -> testProfile.setPlayerId(newPlayerId));
+                () -> testProfile.setPlayerId(playerId), "setPlayerId should throw an exception but didn't");
+            assertTrue(exception.getMessage().contains("playerId is already chosen, chose another one"), "should be true");
     }*/
 }
