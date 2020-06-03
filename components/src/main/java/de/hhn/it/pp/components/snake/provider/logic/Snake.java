3f32c13a0aa031f43760db6ad903441b937a30e3
Karen Hofele <khofele@stud.hs-heilbronn.de>
2020-06-03 23:41:52 +0200

    added constructor and methods to class snake

--- components/src/main/java/de/hhn/it/pp/components/snake/provider/logic/Snake.java
+++ components/src/main/java/de/hhn/it/pp/components/snake/provider/logic/Snake.java
@@ -1,4 +1,27 @@
 package de.hhn.it.pp.components.snake.provider.logic;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Class for the playable snake.
+ *
+ * @author Karen Hofele, Sören Greiner
+ */
 public class Snake {
+    private static final Logger logger = LoggerFactory.getLogger(Snake.class);
+    private int size;
+    private int sizeCounter = 2;
+
+    public Snake() {
+        size = sizeCounter;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize() {
+        size = sizeCounter++;
+    }
 }
