10499d1feaa689231c53c093b9508351fff0f73d
Karen Hofele <khofele@stud.hs-heilbronn.de>
2020-06-03 23:28:43 +0200

    fixed test methods

--- components/src/test/java/de/hhn/it/pp/components/snake/junit/TestSnakeGameGoodCases.java
+++ components/src/test/java/de/hhn/it/pp/components/snake/junit/TestSnakeGameGoodCases.java
@@ -1,5 +1,7 @@
 package de.hhn.it.pp.components.snake.junit;
 
+import de.hhn.it.pp.components.exceptions.IllegalParameterException;
+import de.hhn.it.pp.components.snake.provider.OurSnakeGame;
 import de.hhn.it.pp.components.snake.provider.logic.PlayerProfile;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -11,29 +13,29 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @DisplayName("Tests the Snakeclass with good cases")
 public class TestSnakeGameGoodCases {
-/**
+
     private static final Logger logger = LoggerFactory.getLogger(TestSnakeGameGoodCases.class);
-    private PlayerProfile testProfile = new PlayerProfile("detleft23");
+    private PlayerProfile testProfile;
+    private PlayerProfile testProfile2;
     private int testIdCounter = 2;
-    private SnakeGame testSnakeGame;
+    private OurSnakeGame testSnakeGame;
+
+    public TestSnakeGameGoodCases() throws IllegalParameterException {
+    }
 
     @BeforeEach
-    void setup() {
-        SnakeGame testSnakeGame = new SnakeGame(testProfile);
-        SnakeGame.setIdCounter(2);
+    void setup() throws IllegalParameterException {
+        testProfile = new PlayerProfile("detlef23");
+        testProfile2 = new PlayerProfile("keksi");
+        OurSnakeGame testSnakeGame = new OurSnakeGame(testProfile2);
     }
 
-    @Test
-    @DisplayName("get IdCounter")
-    void testGetIdCounter() {
-        assertEquals(2, SnakeGame.getIdCounter(), "should be 2");
-    }
 
     @Test
     @DisplayName("get Snake's profile")
-    void testGetSnakeProfil() {
-        SnakeGame testSnakeGame = new SnakeGame(testProfile);
+    void testGetSnakeProfil() throws IllegalParameterException {
+        testProfile = new PlayerProfile("detlef23");
+        OurSnakeGame testSnakeGame = new OurSnakeGame(testProfile);
         assertEquals(testProfile, testSnakeGame.getProfile(), "Snake's profile is detlef23");
     }
-    */
 }
