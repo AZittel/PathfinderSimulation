40a808c2db54c7a694a61fd74b34eadfb5574e5f
Karen Hofele <khofele@stud.hs-heilbronn.de>
2020-06-03 15:17:16 +0200

    renamed Level to Map

--- components/src/test/java/de/hhn/it/pp/components/snake/junit/TestMapGoodCases.java
+++ components/src/test/java/de/hhn/it/pp/components/snake/junit/TestMapGoodCases.java
@@ -1,6 +1,6 @@
 package de.hhn.it.pp.components.snake.junit;
 
-import de.hhn.it.pp.components.snake.provider.logic.Level;
+import de.hhn.it.pp.components.snake.provider.logic.Map;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -11,26 +11,26 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 @DisplayName("Tests the level class with good cases")
-public class TestLevelGoodCases {
-    private static final Logger logger = LoggerFactory.getLogger(TestLevelGoodCases.class);
-    private Level testLevel;
+public class TestMapGoodCases {
+    private static final Logger logger = LoggerFactory.getLogger(TestMapGoodCases.class);
+    private Map testMap;
 
     @BeforeEach
     void setup() {
-        Level testLevel = new Level(2, 20);
+        Map testMap = new Map(2, 20);
     }
 
     @Test
     @DisplayName("get level's id")
     void testGetLevelId(){
-        Level testLevel = new Level(2, 20);
-        assertEquals(2, testLevel.getLevelId(), "id should be 2");
+        Map testMap = new Map(2, 20);
+        assertEquals(2, testMap.getLevelId(), "id should be 2");
     }
 
     @Test
     @DisplayName("get needed highscore")
     void testGetNeededHighscore(){
-        Level testLevel = new Level(2, 20);
-        assertEquals(20, testLevel.getNeededHighscore(), "needed highscore should be 20");
+        Map testMap = new Map(2, 20);
+        assertEquals(20, testMap.getNeededHighscore(), "needed highscore should be 20");
     }
 }
