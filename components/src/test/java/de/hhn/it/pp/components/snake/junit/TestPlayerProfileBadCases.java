8c17adfcb5a900f9d4f0f6982352a48a1c581c25
Karen Hofele <khofele@stud.hs-heilbronn.de>
2020-06-03 23:27:51 +0200

    fixed test method testSetPlayerIdBadCase

--- components/src/test/java/de/hhn/it/pp/components/snake/junit/TestPlayerProfileBadCases.java
+++ components/src/test/java/de/hhn/it/pp/components/snake/junit/TestPlayerProfileBadCases.java
@@ -4,31 +4,31 @@ import de.hhn.it.pp.components.exceptions.IllegalParameterException;
 import de.hhn.it.pp.components.snake.provider.logic.PlayerProfile;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@DisplayName("Tests SnakePlayerProfile with bad cases")
+@DisplayName("Tests PlayerProfile with bad cases")
 public class TestPlayerProfileBadCases {
 
     private static final Logger logger = LoggerFactory.getLogger(TestPlayerProfileBadCases.class);
     private PlayerProfile testProfile;
-    private int playerId = 1;
 
     @BeforeEach
     void setup() throws IllegalParameterException {
         PlayerProfile testProfile = new PlayerProfile("walter456");
-        testProfile.setPlayerId(playerId);
+        PlayerProfile testProfile2 = new PlayerProfile("koko");
     }
 
-    //todo Frage: Wieso failed der Test?
-    /**@Test
+    @Test
     @DisplayName("Tests setPlayerId with id that is already taken")
     void testSetPlayerIdBadCase() {
+        testProfile = new PlayerProfile("walter456");
         IllegalParameterException exception = assertThrows(IllegalParameterException.class,
-                () -> testProfile.setPlayerId(playerId), "setPlayerId should throw an exception but didn't");
+                () -> testProfile.setPlayerId(), "setPlayerId should throw an exception but didn't");
             assertTrue(exception.getMessage().contains("playerId is already chosen, chose another one"), "should be true");
-    }*/
+    }
 }
