7c314991117db0c8deadfd530633e55269aeaabe
Karen Hofele <khofele@stud.hs-heilbronn.de>
2020-06-03 14:12:16 +0200

    renamed classes

--- components/src/test/java/de/hhn/it/pp/components/snake/junit/TestAdminInterfaceBadCases.java
+++ components/src/test/java/de/hhn/it/pp/components/snake/junit/TestAdminInterfaceBadCases.java
@@ -1,9 +1,9 @@
 package de.hhn.it.pp.components.snake.junit;
 
-import de.hhn.it.pp.components.snake.SnakePlayerProfile;
-import de.hhn.it.pp.components.snake.SnakePlayerService;
+import de.hhn.it.pp.components.snake.PlayerProfile;
+import de.hhn.it.pp.components.snake.SnakeService;
 import de.hhn.it.pp.components.snake.provider.AdminSnakePlayerService;
-import de.hhn.it.pp.components.snake.provider.OurSnakePlayerService;
+import de.hhn.it.pp.components.snake.provider.OurSnakeService;
 import de.hhn.it.pp.components.exceptions.IllegalParameterException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -18,13 +18,13 @@ public class TestAdminInterfaceBadCases {
     private static final Logger logger =
         LoggerFactory.getLogger(TestAdminInterfaceBadCases.class);
 
-    SnakePlayerService snakePlayerService;
+    SnakeService snakeService;
     AdminSnakePlayerService adminSnakePlayerService;
 
     @BeforeEach
     void setup() {
-        OurSnakePlayerService ourSnakePlayerService = new OurSnakePlayerService();
-        snakePlayerService = ourSnakePlayerService;
+        OurSnakeService ourSnakePlayerService = new OurSnakeService();
+        snakeService = ourSnakePlayerService;
         adminSnakePlayerService = ourSnakePlayerService;
     }
 
@@ -38,7 +38,7 @@ public class TestAdminInterfaceBadCases {
     @Test
     @DisplayName("Test bad creation with null reference as nickname in descriptor")
     public void testCreatePlayerWithDescriptorWithNullLocation() {
-        SnakePlayerProfile descriptor = new SnakePlayerProfile(null);
+        PlayerProfile descriptor = new PlayerProfile(null);
         IllegalParameterException exception = assertThrows(IllegalParameterException.class,
                 () -> adminSnakePlayerService.addSnakePlayerProfile(descriptor));
     }
@@ -46,7 +46,7 @@ public class TestAdminInterfaceBadCases {
     @Test
     @DisplayName("Test bad creation with empty string as nickname in descriptor")
     public void testCreatePlayerWithDescriptorWithEmptyStringLocation() {
-        SnakePlayerProfile descriptor = new SnakePlayerProfile("");
+        PlayerProfile descriptor = new PlayerProfile("");
         IllegalParameterException exception = assertThrows(IllegalParameterException.class,
                 () -> adminSnakePlayerService.addSnakePlayerProfile(descriptor));
     }
@@ -54,7 +54,7 @@ public class TestAdminInterfaceBadCases {
     @Test
     @DisplayName("Test bad creation with just white space as location in descriptor")
     public void testCreatePlayerWithDescriptorWithWhitespaceLocation() {
-        SnakePlayerProfile descriptor = new SnakePlayerProfile("  ");
+        PlayerProfile descriptor = new PlayerProfile("  ");
         IllegalParameterException exception = assertThrows(IllegalParameterException.class,
                 () -> adminSnakePlayerService.addSnakePlayerProfile(descriptor));
     }
