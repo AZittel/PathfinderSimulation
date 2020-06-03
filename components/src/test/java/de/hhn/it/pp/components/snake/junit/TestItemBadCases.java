1cc7ce2845d90bb057b6fdb94e62ae6b56c8d34e
Karen Hofele <khofele@stud.hs-heilbronn.de>
2020-06-03 14:13:09 +0200

    renamed OurSnakeItem to Item

--- components/src/test/java/de/hhn/it/pp/components/snake/junit/TestItemBadCases.java
+++ components/src/test/java/de/hhn/it/pp/components/snake/junit/TestItemBadCases.java
@@ -1,7 +1,7 @@
 package de.hhn.it.pp.components.snake.junit;
 
 import de.hhn.it.pp.components.exceptions.IllegalParameterException;
-import de.hhn.it.pp.components.snake.provider.OurSnakeItem;
+import de.hhn.it.pp.components.snake.provider.Item;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -13,14 +13,14 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 @DisplayName("Tests the item class with bad cases")
 public class TestItemBadCases {
     private static final Logger logger = LoggerFactory.getLogger(TestItemBadCases.class);
-    private OurSnakeItem testItem;
+    private Item testItem;
     private int xPos = 0;
     private int yPos = 0;
 
 
     @BeforeEach
     void setup() {
-        testItem = new OurSnakeItem(2);
+        testItem = new Item(2);
     }
 
     @Test
