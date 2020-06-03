ca384569f857b3b8333017cfd0c7eb614a5d4a87
Karen Hofele <khofele@stud.hs-heilbronn.de>
2020-06-03 14:50:04 +0200

    moved class

--- components/src/main/java/de/hhn/it/pp/components/snake/provider/logic/Item.java
+++ components/src/main/java/de/hhn/it/pp/components/snake/provider/logic/Item.java
@@ -1,6 +1,7 @@
-package de.hhn.it.pp.components.snake.provider;
+package de.hhn.it.pp.components.snake.provider.logic;
 
 import de.hhn.it.pp.components.exceptions.IllegalParameterException;
+import de.hhn.it.pp.components.snake.provider.OurSnakeService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
