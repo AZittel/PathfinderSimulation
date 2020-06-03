86f3d2034abd361a67e1491899865aec1903145a
Karen Hofele <khofele@stud.hs-heilbronn.de>
2020-06-03 14:15:02 +0200

    renamed SnakePlayerProfile to PlayerProfile

--- components/src/test/java/de/hhn/it/pp/components/snake/junit/TestPlayerProfileGoodCases.java
+++ components/src/test/java/de/hhn/it/pp/components/snake/junit/TestPlayerProfileGoodCases.java
@@ -1,7 +1,7 @@
 package de.hhn.it.pp.components.snake.junit;
 
 import de.hhn.it.pp.components.exceptions.IllegalParameterException;
-import de.hhn.it.pp.components.snake.SnakePlayerProfile;
+import de.hhn.it.pp.components.snake.PlayerProfile;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -11,26 +11,26 @@ import org.junit.platform.commons.logging.LoggerFactory;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @DisplayName("Tests the SnakePlayerProfile class with good cases")
-public class TestSnakePlayerProfileGoodCases {
-    private static final Logger logger = LoggerFactory.getLogger(TestSnakePlayerProfileGoodCases.class);
+public class TestPlayerProfileGoodCases {
+    private static final Logger logger = LoggerFactory.getLogger(TestPlayerProfileGoodCases.class);
     private String newNickname = "detlef2";
 
     @BeforeEach
     void setup() {
-        SnakePlayerProfile testProfile = new SnakePlayerProfile("walter45");
+        PlayerProfile testProfile = new PlayerProfile("walter45");
     }
 
     @Test
     @DisplayName("get the player's nickname")
     void testGetPlayerNickname(){
-        SnakePlayerProfile testProfile  = new SnakePlayerProfile("walter45");
+        PlayerProfile testProfile  = new PlayerProfile("walter45");
         assertEquals("walter45", testProfile.getPlayerNickname(), "Nickname should be walter45");
     }
 
     @Test
     @DisplayName("set new nickname")
     void testSetPlayerNickname() {
-        SnakePlayerProfile testProfile  = new SnakePlayerProfile("walter45");
+        PlayerProfile testProfile  = new PlayerProfile("walter45");
         testProfile.setPlayerNickname(newNickname);
         assertEquals("detlef2", testProfile.getPlayerNickname(), "nickname is set to detlef2");
     }
@@ -38,7 +38,7 @@ public class TestSnakePlayerProfileGoodCases {
     @Test
     @DisplayName("get the player's highscore")
     void testGetPlayerHighscore() {
-        SnakePlayerProfile testProfile  = new SnakePlayerProfile("walter45");
+        PlayerProfile testProfile  = new PlayerProfile("walter45");
         testProfile.setPlayerHighscore(46);
         assertEquals(46, testProfile.getPlayerHighscore(), "highscore should be 46");
     }
@@ -46,7 +46,7 @@ public class TestSnakePlayerProfileGoodCases {
     @Test
     @DisplayName("get current level")
     void testGetCurrentLevel() {
-        SnakePlayerProfile testProfile  = new SnakePlayerProfile("walter45");
+        PlayerProfile testProfile  = new PlayerProfile("walter45");
         testProfile.setCurrentLevel(1);
         assertEquals(1, testProfile.getCurrentLevel(), "current level is level 1");
     }
@@ -54,7 +54,7 @@ public class TestSnakePlayerProfileGoodCases {
     @Test
     @DisplayName("get player id")
     void testGetPlayerId() throws IllegalParameterException {
-        SnakePlayerProfile testProfile  = new SnakePlayerProfile("walter45");
+        PlayerProfile testProfile  = new PlayerProfile("walter45");
         testProfile.setPlayerId(2);
         assertEquals(2, testProfile.getPlayerId(), "player's id is 2");
     }
