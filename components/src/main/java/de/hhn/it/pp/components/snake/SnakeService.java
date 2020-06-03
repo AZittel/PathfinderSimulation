41752d3a10d3b23c6273e5701023c3f24d72c454
Karen Hofele <khofele@stud.hs-heilbronn.de>
2020-06-03 14:52:38 +0200

    moved class

--- components/src/main/java/de/hhn/it/pp/components/snake/SnakeService.java
+++ components/src/main/java/de/hhn/it/pp/components/snake/SnakeService.java
@@ -1,6 +1,9 @@
 package de.hhn.it.pp.components.snake;
 
 import de.hhn.it.pp.components.exceptions.IllegalParameterException;
+import de.hhn.it.pp.components.snake.provider.logic.Direction;
+import de.hhn.it.pp.components.snake.provider.logic.PlayerProfile;
+
 import java.util.List;
 
 /**
