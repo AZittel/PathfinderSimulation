d79c91cc4b9b59d2f2953334dab07c7e70e5a0be
Karen Hofele <khofele@stud.hs-heilbronn.de>
2020-06-07 13:13:21 +0200

    added class SnakeTail with getter and setter methods

---
+++ components/src/main/java/de/hhn/it/pp/components/snake/provider/logic/SnakeTail.java
@@ -0,0 +1,43 @@
+package de.hhn.it.pp.components.snake.provider.logic;
+
+public class SnakeTail {
+
+    private int tailXPosition;
+    private int tailYPosition;
+    private boolean wait = true;
+
+    public SnakeTail (int x, int y) {
+        this.tailXPosition = x;
+        this.tailYPosition = y;
+    }
+
+    //todo kommentieren
+    //todo javadoc
+    //todo logger
+
+    public int getTailXPosition() {
+        return tailXPosition;
+    }
+
+    public void setTailXPosition(int tailXPosition) {
+        this.tailXPosition = tailXPosition;
+    }
+
+    public int getTailYPosition() {
+        return tailYPosition;
+    }
+
+    public void setTailYPosition(int tailYPosition) {
+        this.tailYPosition = tailYPosition;
+    }
+
+    public boolean isWait() {
+        return wait;
+    }
+
+    public void setWait(boolean wait) {
+        this.wait = wait;
+    }
+
+
+}
