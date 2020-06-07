ce4ada05e095d57a4414513bd1226a29cfe386af
Karen Hofele <khofele@stud.hs-heilbronn.de>
2020-06-07 13:12:20 +0200

    added class Collision

---
+++ components/src/main/java/de/hhn/it/pp/components/snake/provider/logic/Collision.java
@@ -0,0 +1,7 @@
+package de.hhn.it.pp.components.snake.provider.logic;
+
+public class Collision {
+    //Snake is able to collide with a wall and dies
+    //Snake is able to collide with itself and dies
+    //Snake is able to "collide" with food and grows
+}
