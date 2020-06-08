c68eac7eaf64995c414b8e8782b9f444225f0100
Soeren Greiner <sogreine@stud.hs-heilbronn.de>
2020-06-08 16:34:29 +0200

    Deleted components/src/main/java/de/hhn/it/pp/components/snake/SnakeListener.java, components/src/main/java/de/hhn/it/pp/components/snake/provider/SnakeGame.java, components/src/main/java/de/hhn/it/pp/components/snake/provider/OurSnakeGame.java, components/src/main/java/de/hhn/it/pp/components/snake/provider/states/ControlState.java, components/src/main/java/de/hhn/it/pp/components/snake/provider/logic/PlayerProfile.java, components/src/main/java/de/hhn/it/pp/components/snake/provider/logic/State.java, components/src/main/java/de/hhn/it/pp/components/snake/provider/AdminSnakeService.java files

--- components/src/main/java/de/hhn/it/pp/components/snake/SnakeListener.java
+++
@@ -1,9 +0,0 @@
-package de.hhn.it.pp.components.snake;
-
-import de.hhn.it.pp.components.snake.provider.logic.State;
-
-//todo javadoc
-
-public interface SnakeListener {
-  void newState(State state);
-}
--- components/src/main/java/de/hhn/it/pp/components/snake/provider/logic/State.java
+++
@@ -1,6 +0,0 @@
-package de.hhn.it.pp.components.snake.provider.logic;
-
-//todo javadoc
-public enum State {
-    GAME_ACTIVE, GAME_OVER
-}
