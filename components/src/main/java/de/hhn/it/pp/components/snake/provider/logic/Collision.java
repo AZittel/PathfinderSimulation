95cb06143ae1d271e0dee9627594a551cf3c8acf
Karen Hofele <khofele@stud.hs-heilbronn.de>
2020-06-08 12:20:01 +0200

    added todo

--- components/src/main/java/de/hhn/it/pp/components/snake/provider/logic/Collision.java
+++ components/src/main/java/de/hhn/it/pp/components/snake/provider/logic/Collision.java
@@ -1,5 +1,7 @@
 package de.hhn.it.pp.components.snake.provider.logic;
 
+//todo javadoc
+//todo logger
 public class Collision {
     //Snake is able to collide with a wall and dies
     //Snake is able to collide with itself and dies
