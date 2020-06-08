268377ebb5e30c0f687cd6a3477f53a0f4b4870d
Karen Hofele <khofele@stud.hs-heilbronn.de>
2020-06-08 12:57:04 +0200

    fixed Coding Conventions

--- components/src/main/java/de/hhn/it/pp/components/snake/provider/OurSnakeGame.java
+++ components/src/main/java/de/hhn/it/pp/components/snake/provider/OurSnakeGame.java
@@ -4,114 +4,114 @@ import de.hhn.it.pp.components.exceptions.IllegalParameterException;
 import de.hhn.it.pp.components.snake.SnakeListener;
 import de.hhn.it.pp.components.snake.provider.logic.PlayerProfile;
 import de.hhn.it.pp.components.snake.provider.states.ControlState;
-
 import java.util.ArrayList;
 import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 //todo javadoc
 //todo logging
 
 public class OurSnakeGame implements SnakeGame {
-    private static final org.slf4j.Logger logger =
-            org.slf4j.LoggerFactory.getLogger(OurSnakeGame.class);
-    private static int idCounter;
-    private ControlState controlState;
-    private List<SnakeListener> listeners;
-    private PlayerProfile profile;
+  private static final Logger logger = LoggerFactory.getLogger(OurSnakeGame.class);
+  private static int idCounter;
+  private ControlState controlState;
+  private List<SnakeListener> listeners;
+  private PlayerProfile profile;
 
-    /**
-     * Constructor to create a OurSnake based on the information in the given
-     * SnakePlayerDescriptor.
-     *
-     * @param profile Descriptor with basic facts about the Snake to be created
-     */
-    public OurSnakeGame (PlayerProfile profile) {
-        logger.info("Constructor - {}", profile);
-        listeners = new ArrayList<>();
-        this.profile = profile;
-        //this.profile.setPlayerId(idCounter++);
-        //controlState = new OverState( snake: this);
-        updatePlayerProfile();
+  /**
+   * Constructor to create a OurSnake based on the information in the given
+   * SnakePlayerDescriptor.
+   *
+   * @param profile Descriptor with basic facts about the Snake to be created
+   */
+  public OurSnakeGame(PlayerProfile profile) {
+    logger.info("Constructor - {}", profile);
+    listeners = new ArrayList<>();
+    this.profile = profile;
+    //this.profile.setPlayerId(idCounter++);
+    // controlState = new OverState( snake: this);
+    updatePlayerProfile();
+  }
+
+  public static int getIdCounter() {
+    return idCounter;
+  }
+
+  public static void setIdCounter(final int idCounter) {
+    OurSnakeGame.idCounter = idCounter;
+  }
+
+  private void updatePlayerProfile() {
+    profile.setCurrentState(controlState.getState());
+  }
+
+  @Override
+  public void startGame() throws IllegalStateException {
+    controlState.onStartGame();
+  }
+
+  @Override
+  public void switchLevel() throws IllegalParameterException {
+    controlState.onSwitchLevel();
+  }
+
+  @Override
+  public void endGame() throws IllegalStateException {
+    controlState.onEndGame();
+  }
+
+
+  @Override
+  public void addCallback(SnakeListener listener) throws IllegalParameterException {
+    if (listener == null) {
+      throw new IllegalParameterException("Listener was null reference.");
     }
-
-    public static int getIdCounter() {
-        return idCounter;
+    if (listeners.contains(listener)) {
+      throw new IllegalParameterException("Listener already registered.");
     }
+    listeners.add(listener);
+  }
 
-    public static void setIdCounter(final int idCounter) {
-        OurSnakeGame.idCounter = idCounter;
+  @Override
+  public void removeCallback(SnakeListener listener) throws IllegalParameterException {
+    if (listener == null) {
+      throw new IllegalParameterException("Listener was null reference.");
     }
-
-    private void updatePlayerProfile() {
-        profile.setCurrentState(controlState.getState());
+    if (!listeners.contains(listener)) {
+      throw new IllegalParameterException("Listener is not registered:" + listener);
     }
+    listeners.remove(listener);
+  }
 
-    @Override
-    public void startGame() throws IllegalStateException {
-        controlState.onStartGame();
+  @Override
+  public PlayerProfile getProfile() {
+    return profile;
+  }
+
+  public ControlState getControlState() {
+    return controlState;
+  }
+
+  /**
+   * sets the state of the Snake and notifies all listeners.
+   *
+   * @param controlState new maker state
+   */
+  public void setControlState(final ControlState controlState) {
+    logger.debug("setMakerState - {}", controlState);
+    this.controlState = controlState;
+    updatePlayerProfile();
+    notifyListeners(controlState);
+  }
+
+  /**
+   *
+   * @param controlState
+   */
+  private void notifyListeners(ControlState controlState) {
+    for (SnakeListener listener : listeners) {
+      listener.newState(controlState.getState());
     }
-
-    @Override
-    public void switchLevel() throws IllegalParameterException {
-        controlState.onSwitchLevel();
-    }
-
-    @Override
-    public void endGame() throws IllegalStateException {
-        controlState.onEndGame();
-    }
-
-
-    @Override
-    public void addCallback(SnakeListener listener) throws IllegalParameterException {
-        if (listener == null) {
-            throw new IllegalParameterException("Listener was null reference.");
-        }
-        if (listeners.contains(listener)) {
-            throw new IllegalParameterException("Listener already registered.");
-        }
-        listeners.add(listener);
-    }
-
-    @Override
-    public void removeCallback(SnakeListener listener) throws IllegalParameterException {
-        if (listener == null) {
-            throw new IllegalParameterException("Listener was null reference.");
-        }
-        if (!listeners.contains(listener)) {
-            throw new IllegalParameterException("Listener is not registered:" + listener);
-        }
-        listeners.remove(listener);
-    }
-
-    @Override
-    public PlayerProfile getProfile() {
-        return profile;
-    }
-
-    public ControlState getControlState() {
-        return controlState;
-    }
-
-    /**
-     * sets the state of the Snake and notifies all listeners.
-     *
-     * @param controlState new maker state
-     */
-    public void setControlState(final ControlState controlState) {
-        logger.debug("setMakerState - {}", controlState);
-        this.controlState = controlState;
-        updatePlayerProfile();
-        notifyListeners(controlState);
-    }
-
-    /**
-     *
-     * @param controlState
-     */
-    private void notifyListeners(ControlState controlState) {
-        for (SnakeListener listener : listeners) {
-            listener.newState(controlState.getState());
-        }
-    }
+  }
 }
