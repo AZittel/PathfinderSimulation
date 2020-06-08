176735b3116b9cf4ac3a05da2e9b06979543219c
Karen Hofele <khofele@stud.hs-heilbronn.de>
2020-06-08 12:57:24 +0200

    fixed Coding Conventions

--- components/src/main/java/de/hhn/it/pp/components/snake/provider/SnakeGame.java
+++ components/src/main/java/de/hhn/it/pp/components/snake/provider/SnakeGame.java
@@ -7,15 +7,15 @@ import de.hhn.it.pp.components.snake.provider.logic.PlayerProfile;
 
 //todo javadoc
 public interface SnakeGame {
-    void startGame() throws IllegalStateException;
+  void startGame() throws IllegalStateException;
 
-    void switchLevel() throws IllegalParameterException;
+  void switchLevel() throws IllegalParameterException;
 
-    void endGame() throws IllegalStateException;
+  void endGame() throws IllegalStateException;
 
-    void addCallback(SnakeListener listener) throws IllegalParameterException;
+  void addCallback(SnakeListener listener) throws IllegalParameterException;
 
-    void removeCallback(SnakeListener listener) throws IllegalParameterException;
+  void removeCallback(SnakeListener listener) throws IllegalParameterException;
 
-    PlayerProfile getProfile();
+  PlayerProfile getProfile();
 }
