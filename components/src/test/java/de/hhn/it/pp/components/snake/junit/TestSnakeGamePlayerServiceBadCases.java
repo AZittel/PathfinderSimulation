3e4695363d61ea131af5fddd65331c223da1a7ae
Karen Hofele <khofele@stud.hs-heilbronn.de>
2020-06-08 18:03:58 +0200

    deleted imports deleted test methods

--- components/src/test/java/de/hhn/it/pp/components/snake/junit/TestSnakeGamePlayerServiceBadCases.java
+++ components/src/test/java/de/hhn/it/pp/components/snake/junit/TestSnakeGamePlayerServiceBadCases.java
@@ -1,8 +1,5 @@
 package de.hhn.it.pp.components.snake.junit;
-
-import de.hhn.it.pp.components.snake.provider.logic.PlayerProfile;
 import de.hhn.it.pp.components.snake.SnakeService;
-import de.hhn.it.pp.components.snake.provider.AdminSnakeService;
 import de.hhn.it.pp.components.snake.provider.OurSnakeService;
 import de.hhn.it.pp.components.exceptions.IllegalParameterException;
 import org.junit.jupiter.api.BeforeEach;
@@ -10,9 +7,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 //todo not testable right now
 
@@ -20,24 +14,6 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 public class TestSnakeGamePlayerServiceBadCases {
     private static final Logger logger =
             LoggerFactory.getLogger(TestSnakeGamePlayerServiceBadCases.class);
-
-    SnakeService snakeService;
-    AdminSnakeService adminSnakeService;
-
-    @BeforeEach
-    void setup(List<PlayerProfile> descriptors) throws IllegalParameterException {
-        OurSnakeService ourSnakePlayerService = new OurSnakeService();
-        snakeService = ourSnakePlayerService;
-        adminSnakeService = ourSnakePlayerService;
-    }
-
-    /**@Test
-    @DisplayName("ask for a non existing Player")
-    void testExceptionWhenRequestingNonExistentPlayer() {
-        IllegalParameterException illegalParameterException = assertThrows(
-                IllegalParameterException.class,
-                () -> snakePlayerService.getSnakePlayerProfile(23));
-    }*/
 
 
 }
