8391c7c6978b6a8383b5735b3c41d4f038fa8dd1
Karen Hofele <khofele@stud.hs-heilbronn.de>
2020-06-08 12:21:19 +0200

    added todo

--- components/src/main/java/de/hhn/it/pp/components/snake/SnakeListener.java
+++ components/src/main/java/de/hhn/it/pp/components/snake/SnakeListener.java
@@ -2,6 +2,8 @@ package de.hhn.it.pp.components.snake;
 
 import de.hhn.it.pp.components.snake.provider.logic.State;
 
+//todo javadoc
+
 public interface SnakeListener {
     void newState(State state);
 }
