07ff5bbe166ce549b54661b3e2b73a9a86518567
Karen Hofele <khofele@stud.hs-heilbronn.de>
2020-06-11 23:17:28 +0200

    implemented class Collision

--- components/src/main/java/de/hhn/it/pp/components/snake/provider/logic/Collision.java
+++ components/src/main/java/de/hhn/it/pp/components/snake/provider/logic/Collision.java
@@ -3,7 +3,29 @@ package de.hhn.it.pp.components.snake.provider.logic;
 //todo javadoc
 //todo logger
 public class Collision {
-    //Snake is able to collide with a wall and dies
-    //Snake is able to collide with itself and dies
-    //Snake is able to "collide" with food and grows
-}
+    //Snake is able to collide with a wall and dies --> later, nice to have
+
+
+    public static boolean collideWithSnake() {
+        for (int i = 0; i <= Snake.allTails.size(); i++) {
+            if (Snake.snakeHead.getHeadXPosition() == Snake.allTails.get(i).getTailXPosition() && Snake.snakeHead.getHeadYPosition() == Snake.allTails.get(i).getTailYPosition() && !Snake.allTails.get(i).isWait()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+
+        public static void collideWithItem () {
+            if (Snake.snakeHead.getHeadXPosition() == Snake.collectibleItem.getxPosition() && Snake.snakeHead.getHeadYPosition() == Snake.collectibleItem.getyPosition()) {
+                Snake.collectibleItem.reset();
+                Snake.addTail();
+                Snake.score += Snake.collectibleItem.getValue();
+                if(Snake.score > Snake.highscore) {
+                    Snake.highscore = Snake.score;
+                }
+            }
+        }
+    }
+
