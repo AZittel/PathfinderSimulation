d2231ec2a990e8c1c300a0e628dbdb130cf6017f
Karen Hofele <khofele@stud.hs-heilbronn.de>
2020-06-14 16:13:12 +0200

    added test method

--- components/src/test/java/de/hhn/it/pp/components/snake/junit/TestCollision.java
+++ components/src/test/java/de/hhn/it/pp/components/snake/junit/TestCollision.java
@@ -1,4 +1,30 @@
 package de.hhn.it.pp.components.snake.junit;
 
+import de.hhn.it.pp.components.snake.provider.logic.Collision;
+import de.hhn.it.pp.components.snake.provider.logic.Snake;
+import de.hhn.it.pp.components.snake.provider.logic.SnakeTail;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class TestCollision {
+
+    @Test
+    @DisplayName("test collide with snake")
+    void testCollideWithSnake() {
+        SnakeTail testTail = new SnakeTail(15,15);
+        testTail.setWait(false);
+        Snake.allTails.add(testTail);
+        assertEquals(true, Collision.collideWithSnake(), "should be true");
+    }
+
+    @Test
+    @DisplayName("test collide with item")
+    void testCollideWithItem() {
+        Snake.collectibleItem.setXPosition(15);
+        Snake.collectibleItem.setYPosition(15);
+        Collision.collideWithItem();
+        assertEquals(1, Snake.getScore(), "should be 1");
+    }
 }
