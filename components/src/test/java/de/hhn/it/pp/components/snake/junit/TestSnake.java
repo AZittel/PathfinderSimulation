4a641d8bf680f20d67e4ce1bc9222149f84377e5
Karen Hofele <khofele@stud.hs-heilbronn.de>
2020-06-14 16:32:53 +0200

    added test methods

--- components/src/test/java/de/hhn/it/pp/components/snake/junit/TestSnake.java
+++ components/src/test/java/de/hhn/it/pp/components/snake/junit/TestSnake.java
@@ -1,4 +1,25 @@
 package de.hhn.it.pp.components.snake.junit;
 
+import de.hhn.it.pp.components.snake.provider.logic.Snake;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@DisplayName("Tests the snake class")
 public class TestSnake {
+
+    @Test
+    @DisplayName("add Tail to Snake")
+    void testAddTail() {
+        Snake.addTail();
+        int size = Snake.allTails.size();
+        assertEquals(size, Snake.allTails.size(), "should be 1");
+    }
+
+    @Test
+    @DisplayName("moves snake")
+    void testMoveSnake() {
+
+    }
 }
