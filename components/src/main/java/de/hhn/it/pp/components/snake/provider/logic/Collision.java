289c80d3965bcbd34e623d335c6be8faf34318db
Karen Hofele <khofele@stud.hs-heilbronn.de>
2020-06-14 17:34:29 +0200

    added logger added javadoc

--- components/src/main/java/de/hhn/it/pp/components/snake/provider/logic/Collision.java
+++ components/src/main/java/de/hhn/it/pp/components/snake/provider/logic/Collision.java
@@ -1,12 +1,21 @@
 package de.hhn.it.pp.components.snake.provider.logic;
 
-//todo javadoc
-//todo logger
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Collision {
     //Snake is able to collide with a wall and dies --> later, nice to have
+    /** Collision's Logger. */
+    private static final Logger logger = LoggerFactory.getLogger(Collision.class);
 
-
+    /**
+     * Snake collides with itself.
+     *
+     * @return true if snake collides with itself
+     * @return false if snake doesn't collide with itself
+     */
     public static boolean collideWithSnake() {
+        logger.info("Snake collides with itself");
         for (int i = 0; i < Snake.allTails.size(); i++) {
             if (Snake.snakeHead.getHeadXPosition() == Snake.allTails.get(i).getTailXPosition() && Snake.snakeHead.getHeadYPosition() == Snake.allTails.get(i).getTailYPosition() && !Snake.allTails.get(i).isWait()) {
                 return true;
@@ -15,9 +24,11 @@ public class Collision {
         return false;
     }
 
-
-
+    /**
+     * Snake collides with item.
+     */
     public static void collideWithItem () {
+        logger.info("Snake 'collided' with item");
         if (Snake.snakeHead.getHeadXPosition() == Snake.collectibleItem.getXPosition() && Snake.snakeHead.getHeadYPosition() == Snake.collectibleItem.getYPosition()) {
             Snake.collectibleItem.reset();
             Snake.addTail();
