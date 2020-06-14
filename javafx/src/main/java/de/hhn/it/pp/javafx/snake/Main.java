88fc86e348a7b00cd98f2d5757ba221c7de6860d
Karen Hofele <khofele@stud.hs-heilbronn.de>
2020-06-14 18:04:30 +0200

    fixed code after coding conventions

--- javafx/src/main/java/de/hhn/it/pp/javafx/snake/Main.java
+++ javafx/src/main/java/de/hhn/it/pp/javafx/snake/Main.java
@@ -5,12 +5,12 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+  public static void main(String[] args) {
+    launch(args);
+  }
 
-    @Override
-    public void start(Stage primaryStage) {
+  @Override
+  public void start(Stage primaryStage) {
 
-    }
+  }
 }
