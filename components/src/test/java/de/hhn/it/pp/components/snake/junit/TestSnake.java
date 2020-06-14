f5c956200310f870ad45f8b3102f78c9592e317f
Karen Hofele <khofele@stud.hs-heilbronn.de>
2020-06-14 16:46:41 +0200

    added testmethod and todo

--- components/src/test/java/de/hhn/it/pp/components/snake/junit/TestSnake.java
+++ components/src/test/java/de/hhn/it/pp/components/snake/junit/TestSnake.java
@@ -1,5 +1,6 @@
 package de.hhn.it.pp.components.snake.junit;
 
+import de.hhn.it.pp.components.snake.provider.logic.Direction;
 import de.hhn.it.pp.components.snake.provider.logic.Snake;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -20,6 +21,14 @@ public class TestSnake {
     @Test
     @DisplayName("moves snake")
     void testMoveSnake() {
-
+        //Startdirection: UP
+        //Start-Coordinates: 15,15
+        //todo Frage: Wie auf 80% Coverage bringen?
+        assertEquals(Direction.UP, Snake.snakeHead.getHeadDirection(), "should be UP");
+        Snake.snakeHead.setHeadDirection(Direction.LEFT);
+        Snake.move();
+        assertEquals(Direction.LEFT, Snake.snakeHead.getHeadDirection(), "should be UP");
+        assertEquals(15, Snake.snakeHead.getHeadYPosition(), "should be Y=14");
+        assertEquals(14, Snake.snakeHead.getHeadXPosition(), "should be X=15");
     }
 }
