7dddb8d02ea656f7adf623dd2b29c8839e160723
Amadeus Zittel <azittel@stud.hs-heilbronn.de>
2020-06-27 13:38:55 +0200

    Added Testcases for diagonal pathing enabled

--- components/src/test/java/de/hhn/it/pp/components/astarpathfinding/junit/AStarPathfindingAlgorithmTest.java
+++ components/src/test/java/de/hhn/it/pp/components/astarpathfinding/junit/AStarPathfindingAlgorithmTest.java
@@ -11,6 +11,7 @@ import de.hhn.it.pp.components.astarpathfinding.exceptions.OccupiedPositionExcep
 import de.hhn.it.pp.components.astarpathfinding.exceptions.PositionOutOfBounds;
 import de.hhn.it.pp.components.astarpathfinding.provider.AStarPathfindingAlgorithm;
 import de.hhn.it.pp.components.astarpathfinding.provider.MapManager;
+import de.hhn.it.pp.components.astarpathfinding.provider.Pathfinder;
 import de.hhn.it.pp.components.astarpathfinding.provider.Terrain;
 import de.hhn.it.pp.components.exceptions.IllegalParameterException;
 import java.util.ArrayList;
@@ -22,16 +23,17 @@ import org.junit.jupiter.api.Test;
 
 public class AStarPathfindingAlgorithmTest {
   private static final org.slf4j.Logger logger =
-      org.slf4j.LoggerFactory.getLogger(AStarPathfindingAlgorithmTest.class);
+    org.slf4j.LoggerFactory.getLogger(AStarPathfindingAlgorithmTest.class);
 
   private MapManager testMapManager;
   private AStarPathfindingAlgorithm algorithm;
+  private AStarPathfindingAlgorithm algorithmDiagonal;
 
   @BeforeEach
   void setup() {
     testMapManager = new MapManager();
-
     algorithm = new AStarPathfindingAlgorithm(testMapManager, false);
+    algorithmDiagonal = new AStarPathfindingAlgorithm(testMapManager, true);
   }
 
   @Nested
@@ -41,15 +43,15 @@ public class AStarPathfindingAlgorithmTest {
     @Test
     @DisplayName("Find the final and fastest path")
     public void doPathfinding_goodResult()
-        throws PositionOutOfBounds, OccupiedPositionException, IllegalParameterException {
+      throws PositionOutOfBounds, OccupiedPositionException, IllegalParameterException {
       Position start = new Position(2, 2);
       testMapManager.setStartCoordinates(start);
       Position end = new Position(2, 6);
       testMapManager.setDestinationCoordinates(end);
       testMapManager.createMap(7, 7);
-      Terrain terrain2 = testMapManager.createTerrain(TerrainType.SWAMP, new Position(1, 4));
-      Terrain terrain1 = testMapManager.createTerrain(TerrainType.LAVA, new Position(2, 4));
-      Terrain terrain3 = testMapManager.createTerrain(TerrainType.SWAMP, new Position(3, 4));
+      testMapManager.createTerrain(TerrainType.SWAMP, new Position(1, 4));
+      testMapManager.createTerrain(TerrainType.LAVA, new Position(2, 4));
+      testMapManager.createTerrain(TerrainType.SWAMP, new Position(3, 4));
       ArrayList<PathfindingInformation> result = algorithm.findPath();
       List<Terrain> finalPath = result.get(result.size() - 1).getFinalPathPositions();
       printMapWithFinalPath(finalPath);
@@ -97,7 +99,7 @@ public class AStarPathfindingAlgorithmTest {
     @Test
     @DisplayName("Influence pathfinding with changed modifiers")
     public void doPathfinding_changedTerrainTypeModifier_GoodResult()
-        throws OccupiedPositionException, PositionOutOfBounds, IllegalParameterException {
+      throws OccupiedPositionException, PositionOutOfBounds, IllegalParameterException {
       testMapManager.createMap(7, 7);
       testMapManager.createTerrain(TerrainType.SWAMP, new Position(1, 4));
       testMapManager.createTerrain(TerrainType.LAVA, new Position(2, 4));
@@ -137,6 +139,34 @@ public class AStarPathfindingAlgorithmTest {
       assertFalse(finalPath.isEmpty());
       assertEquals(expectedPath, finalPath, "The 2. expected path is not the same as the actual path");
     }
+
+    @Test
+    @DisplayName("Check pathfinding with diagonal pathing enabled")
+    public void doPathfinding_diagonalPathingEnabled_GoodResult() throws PositionOutOfBounds, OccupiedPositionException, IllegalParameterException {
+      testMapManager.createMap(5, 7);
+      testMapManager.setStartCoordinates(new Position(0, 0));
+      testMapManager.setDestinationCoordinates(new Position(6, 4));
+      testMapManager.createTerrain(TerrainType.LAVA, new Position(0,1));
+      testMapManager.createTerrain(TerrainType.LAVA, new Position(1,1));
+      testMapManager.createTerrain(TerrainType.LAVA, new Position(2,1));
+      testMapManager.createTerrain(TerrainType.LAVA, new Position(3,0));
+
+      ArrayList<PathfindingInformation> result = algorithmDiagonal.findPath();
+      List<Terrain> finalPath = result.get(result.size() - 1).getFinalPathPositions();
+      printMapWithFinalPath(finalPath);
+
+      List<Terrain> expectedPath = new ArrayList<>();
+      expectedPath.add(testMapManager.getTerrainAt(new Position(0, 0)));
+      expectedPath.add(testMapManager.getTerrainAt(new Position(1, 0)));
+      expectedPath.add(testMapManager.getTerrainAt(new Position(2, 0)));
+      expectedPath.add(testMapManager.getTerrainAt(new Position(3, 1)));
+      expectedPath.add(testMapManager.getTerrainAt(new Position(4, 2)));
+      expectedPath.add(testMapManager.getTerrainAt(new Position(5, 3)));
+      expectedPath.add(testMapManager.getTerrainAt(new Position(6, 4)));
+
+      assertFalse(finalPath.isEmpty());
+      assertEquals(expectedPath, finalPath, "The expected path is not the same as the actual path");
+    }
   }
 
   @Nested
@@ -151,10 +181,10 @@ public class AStarPathfindingAlgorithmTest {
       testMapManager.setStartCoordinates(start);
 
       // Start point surrounded with lava
-      Terrain terrain2 = testMapManager.createTerrain(TerrainType.LAVA, new Position(1, 2));
-      Terrain terrain1 = testMapManager.createTerrain(TerrainType.LAVA, new Position(2, 1));
-      Terrain terrain3 = testMapManager.createTerrain(TerrainType.LAVA, new Position(2, 3));
-      Terrain terrain4 = testMapManager.createTerrain(TerrainType.LAVA, new Position(3, 2));
+      testMapManager.createTerrain(TerrainType.LAVA, new Position(1, 2));
+      testMapManager.createTerrain(TerrainType.LAVA, new Position(2, 1));
+      testMapManager.createTerrain(TerrainType.LAVA, new Position(2, 3));
+      testMapManager.createTerrain(TerrainType.LAVA, new Position(3, 2));
 
       ArrayList<PathfindingInformation> result = algorithm.findPath();
       assertTrue(result.isEmpty(), "There should not be a path from the start position");
@@ -167,14 +197,30 @@ public class AStarPathfindingAlgorithmTest {
       testMapManager.setDestinationCoordinates(end);
 
       // End point surrounded with lava
-      terrain2 = testMapManager.createTerrain(TerrainType.LAVA, new Position(1, 6));
-      terrain1 = testMapManager.createTerrain(TerrainType.LAVA, new Position(3, 6));
-      terrain3 = testMapManager.createTerrain(TerrainType.LAVA, new Position(2, 7));
-      terrain4 = testMapManager.createTerrain(TerrainType.LAVA, new Position(2, 5));
+      testMapManager.createTerrain(TerrainType.LAVA, new Position(1, 6));
+      testMapManager.createTerrain(TerrainType.LAVA, new Position(3, 6));
+      testMapManager.createTerrain(TerrainType.LAVA, new Position(2, 7));
+      testMapManager.createTerrain(TerrainType.LAVA, new Position(2, 5));
 
       result = algorithm.findPath();
       assertTrue(result.isEmpty(), "There should not be a path to the end position");
     }
+
+    @Test
+    @DisplayName("Check pathfinding with diagonal pathing enabled and start sourrounded")
+    public void doPathfinding_diagonalPathingEnabled_StartPointSourrounded_BadResult()
+      throws PositionOutOfBounds, OccupiedPositionException, IllegalParameterException {
+      testMapManager.createMap(5, 7);
+      testMapManager.setStartCoordinates(new Position(0, 0));
+      testMapManager.setDestinationCoordinates(new Position(6, 4));
+      testMapManager.createTerrain(TerrainType.LAVA, new Position(0,1));
+      testMapManager.createTerrain(TerrainType.LAVA, new Position(1,0));
+      testMapManager.createTerrain(TerrainType.LAVA, new Position(1,1));
+
+      ArrayList<PathfindingInformation> result = algorithmDiagonal.findPath();
+      assertTrue(result.isEmpty(), "There should not be a path to the end position");
+    }
+
   }
 
   private void printMapWithFinalPath(List<Terrain> finalPath) {
