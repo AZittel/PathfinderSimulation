4137f9aaa3f37c6d8833a4a63aa70a6f314161ae
Oliver Koch <okoch@stud.hs-heilbronn.de>
2020-06-28 18:05:06 +0200

    Updated craft-method and the listeners to the new exception

--- components/src/main/java/de/hhn/it/pp/components/craftingservice/provider/CraftingImplementation.java
+++ components/src/main/java/de/hhn/it/pp/components/craftingservice/provider/CraftingImplementation.java
@@ -4,8 +4,8 @@ import de.hhn.it.pp.components.craftingservice.CraftingListener;
 import de.hhn.it.pp.components.craftingservice.CraftingPattern;
 import de.hhn.it.pp.components.craftingservice.CraftingService;
 import de.hhn.it.pp.components.craftingservice.Inventory;
-import de.hhn.it.pp.components.craftingservice.Item;
 import de.hhn.it.pp.components.craftingservice.exceptions.CraftingNotPossibleException;
+import de.hhn.it.pp.components.craftingservice.exceptions.NoActiveListenerException;
 import de.hhn.it.pp.components.exceptions.IllegalParameterException;
 import de.hhn.it.pp.components.exceptions.OperationNotSupportedException;
 
@@ -19,11 +19,11 @@ import java.util.Map;
  * Contains everything which is needed to craft new objects.
  *
  * @author Oliver Koch, Philipp Alessandrini
- * @version 2020-06-11
+ * @version 2020-06-27
  */
 public class CraftingImplementation implements CraftingService {
   private static final org.slf4j.Logger logger =
-      org.slf4j.LoggerFactory.getLogger(CraftingImplementation.class);
+          org.slf4j.LoggerFactory.getLogger(CraftingImplementation.class);
   // stores all crafting patterns
   private Map<String, CraftingPattern> craftingPatterns;
   // initialize the callback notificator
@@ -53,7 +53,7 @@ public class CraftingImplementation implements CraftingService {
 
   @Override
   public void removeCraftingPattern(String patternName) throws IllegalParameterException,
-      OperationNotSupportedException {
+          OperationNotSupportedException {
     if (patternName == null) {
       throw new IllegalParameterException("Pattern-name was null reference!\n");
     }
@@ -87,8 +87,8 @@ public class CraftingImplementation implements CraftingService {
   }
 
   @Override
-  public void craft(
-      Inventory inventory, CraftingPattern craftingPattern) throws CraftingNotPossibleException {
+  public void craft(Inventory inventory, CraftingPattern craftingPattern)
+          throws CraftingNotPossibleException {
     logger.info("craft: inventory = {}, craftingPattern = {}", inventory, craftingPattern);
     if (craftingActive) {
       throw new CraftingNotPossibleException("An item is already being crafted!\n");
@@ -96,26 +96,23 @@ public class CraftingImplementation implements CraftingService {
     if (inventory == null || craftingPattern == null) {
       throw new CraftingNotPossibleException("Inventory or Crafting Pattern was null reference!\n");
     }
-    ArrayList<Item> usedItems = new ArrayList<>();
     // check if the inventory matches the crafting pattern
-    for (int i = 0; i < craftingPattern.getNeededItems().size(); i++) {
-      for (int j = 0; j < inventory.getItems().size(); j++) {
-        if (craftingPattern.getNeededItems().get(i).toString()
-            .equals(inventory.getItems().get(j).toString())) {
-          usedItems.add(inventory.getItems().get(j));
-          try {
-            inventory.remove(inventory.getItems().get(j));
-          } catch (IllegalParameterException | OperationNotSupportedException e) {
-            logger.error(e.getMessage());
+    if (craftingPattern.isCraftable(inventory)) {
+      // if yes: delete needed items from the inventory ...
+      for (int i = 0; i < craftingPattern.getNeededItems().size(); i++) {
+        for (int j = 0; j < inventory.getItems().size(); j++) {
+          if (craftingPattern.getNeededItems().get(i).toString()
+                  .equals(inventory.getItems().get(j).toString())) {
+            try {
+              inventory.remove(inventory.getItems().get(j));
+            } catch (IllegalParameterException | OperationNotSupportedException e) {
+              logger.error(e.getMessage());
+            }
+            j = inventory.getItems().size();
           }
-          j = inventory.getItems().size();
         }
       }
-    }
-    // if the pattern implies the inventory:
-    // create the crafted object/s and add it/them to the inventory
-    if (craftingPattern.getNeededItems().size() == usedItems.size()) {
-      // start crafting process (callback notification)
+      // ... and start the crafting process
       try {
         addListener(callbackNotificator);
         listener.craftingStartedNotification(craftingPattern);
@@ -130,9 +127,8 @@ public class CraftingImplementation implements CraftingService {
       craftingThread.start();
       currentThread = craftingThread;
     } else { // if not: throw exception
-      inventory.getItems().addAll(usedItems);
       throw new CraftingNotPossibleException("Items in inventory don't match '"
-          + craftingPattern.toString() + "'!\n");
+              + craftingPattern.toString() + "'!\n");
     }
   }
 
@@ -157,20 +153,24 @@ public class CraftingImplementation implements CraftingService {
     CraftingImplementation.listener = null;
   }
 
+  /**
+   * Gets the current crafting listener, if there is one.
+   * @return the current crafting listener
+   * @throws NoActiveListenerException if listener = null
+   */
+  public static CraftingListener getListener() throws NoActiveListenerException {
+    if (listener == null) {
+      throw new NoActiveListenerException();
+    }
+    return listener;
+  }
+
   public static CraftingListener getCallbackNotificator() {
     return callbackNotificator;
   }
 
-  public static CraftingListener getListener() {
-    return listener;
-  }
-
   public static Thread getCurrentThread() {
     return currentThread;
-  }
-
-  public static boolean isCraftingActive() {
-    return craftingActive;
   }
 
   public static void setCraftingActive(boolean crafting) {
