cd99b244002af5eaf703dca160e95378ed29803d
Oliver Koch <okoch@stud.hs-heilbronn.de>
2020-06-28 17:59:43 +0200

    Added a exception when there is no active listener

---
+++ components/src/main/java/de/hhn/it/pp/components/craftingservice/exceptions/NoActiveListenerException.java
@@ -0,0 +1,16 @@
+package de.hhn.it.pp.components.craftingservice.exceptions;
+
+/**
+ * An exception related to the crafting listener.
+ *
+ * @author Oliver Koch, Philipp Alessandrini
+ * @version 2020-06-28
+ */
+public class NoActiveListenerException extends Exception {
+
+    /**
+     * If there is no active listener.
+     */
+    public NoActiveListenerException() {
+    }
+}
